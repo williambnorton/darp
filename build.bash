34bfb9f96ff106861601bf747a3426a1d9be8a4f
William B. Norton <wbn@drpeering.net>
2020-03-03 18:43:50 -0800

    ./build.bash working on CI

--- build.bash
+++ build.bash
@@ -1,2 +1,5 @@
 #!/bin/bash
+echo `date` compiling typescript into javascript 
 tsc express/express && tsc pulser/pulser && tsc handlepulse/handlepulse && git add . && git commit -m "$0 working on CI" && git push
+echo `date` Completed compiles and git push
+
