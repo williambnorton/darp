9ccd23beb05aa42efc76c6de7a172f67befc9253
William B. Norton <wbn@drpeering.net>
2020-03-04 13:51:18 -0800

    ./builddarp.bash working on CI

--- config/config.js
+++ config/config.js
@@ -1,23 +1,6 @@
 "use strict";
 exports.__esModule = true;
-//
-//  config.ts - initialize the redis database
-//
-//  me - my internal state and pointer to genesis
-//
-var lib_1 = require("../lib/lib");
 var http = require('http');
-module.exports.me = {
-    "geo": "",
-    "port": 65013,
-    "ipaddr": "",
-    "publickey": "",
-    "mint": "",
-    "bootTime": lib_1.now(),
-    "group": "",
-    "pulseGroups": "",
-    "genesis": ""
-};
 var pulseRedis = require('redis');
 var redisClient = pulseRedis.createClient(); //creates a new client
 redisClient.flushall();
@@ -103,14 +86,14 @@ function setME() {
         console.log(e.message);
     });
 }
-var GEO = process.env.HOSTNAME || require('os').hostname();
+var GEO = process.env.HOSTNAME; // || require('os').hostname();
 var PUBLICKEY = process.env.PUBLICKEY || "";
 var WALLET = process.env.WALLET || "584e560b06717ae0d76b8067d68a2ffd34d7a390f2b2888f83bc9d15462c04b2";
-GEO = GEO.split(".");
+//GEO=GEO.toString().split('.').split(',');
+console.log("config GEO=" + GEO + " publickey=" + PUBLICKEY + " WALLET=" + WALLET + "");
 if (PUBLICKEY == "")
     Usage();
 setME();
-console.log("config DONE");
 function Usage() {
     console.log("usage: node config publickey [geo]");
     process.exit(127);
--- config/config.ts
+++ config/config.ts
@@ -6,17 +6,7 @@
 import { dump, now, me } from "../lib/lib";
 
 var http = require('http');
-module.exports.me = {
-   "geo" : "",
-   "port" : 65013,
-   "ipaddr" : "",   //set by genesis node on connection
-   "publickey" : "",
-    "mint": "",      //set by genesis node
-   "bootTime": now(),
-   "group": "",
-    "pulseGroups" : "",  //list of groups I will pulse
-    "genesis" : ""
-}
+
 
 const pulseRedis = require('redis');
 var redisClient = pulseRedis.createClient(); //creates a new client
@@ -110,15 +100,17 @@ function setME() {
     });
 }
 
-var GEO=process.env.HOSTNAME||require('os').hostname();
-var PUBLICKEY=process.env.PUBLICKEY||"";
-var WALLET=process.env.WALLET||"584e560b06717ae0d76b8067d68a2ffd34d7a390f2b2888f83bc9d15462c04b2";
+var GEO=process.env.HOSTNAME; // || require('os').hostname();
+var PUBLICKEY=process.env.PUBLICKEY || "";
+var WALLET=process.env.WALLET || "584e560b06717ae0d76b8067d68a2ffd34d7a390f2b2888f83bc9d15462c04b2";
 
-GEO=GEO.split(".");
+//GEO=GEO.toString().split('.').split(',');
+
+console.log("config GEO="+GEO+" publickey="+PUBLICKEY+" WALLET="+WALLET+"");
+
 if (PUBLICKEY=="") Usage();
 setME();
 
-console.log("config DONE");
 function Usage() {
     console.log("usage: node config publickey [geo]");
     process.exit( 127 );
