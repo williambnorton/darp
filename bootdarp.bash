90dc6ce97be1b99df9b42579afeccc7840c215c7
William B. Norton <wbn@drpeering.net>
2020-03-04 19:43:11 -0800

    ./builddarp.bash working on CI

--- bootdarp.bash
+++ bootdarp.bash
@@ -1,14 +1,22 @@
 #!/bin/bash
 #		boot
 echo `date` Starting $0 script
-cd /
-mv /darp /tmp
-rm -rf /tmp/darp
-git clone https://github.com/williambnorton/darp.git /root/darp
-ln -s /root/darp /darp
+#cd /
+#mv /darp /tmp
+#rm -rf /tmp/darp
+#git clone https://github.com/williambnorton/darp.git /root/darp
+#ln -s /root/darp /darp
+
+      rm -rf /tmp/darp
+        mv /darp /tmp/darp
+        git clone https://github.com/williambnorton/darp.git    /darp     
+        #mv /darp /tmp
+        #.ln -s /root/darp /darp
+        echo `date` new darp installed
+        ls -l /darp
 
 echo `date` Launching forever script
 ls -l /darp
 cd /darp/scripts
-./forever.bash  #Start the system
+#./forever.bash  #Start the system
 
