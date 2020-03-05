1d6e5c36d64fc27c37b0059e5d27364d288fbcab
William B. Norton <wbn@drpeering.net>
2020-03-05 08:20:36 -0800

    ./builddarp.bash working on CI

--- bootdarp.bash
+++ bootdarp.bash
@@ -1,21 +1,22 @@
 #!/bin/bash
 #		    bootdarp.bash - fetch updated darp software and launch forever script
 #
+cd /tmp
 rm -rf /tmp/darp
 mv /darp /tmp/darp
+echo `date` Cloning new darp code from github
 git clone https://github.com/williambnorton/darp.git    /darp     
 cd /darp
-echo `date` new darp version `ls build*` installed
+echo `date` New darp version: `ls build*` installed
 ls -l /darp
 
+#temp to scaffolding to help build
 cd /darp
 redis-server &  #temp to help building
 cd /darp/express
 node express &
 
-
-
-echo `date` Launching forever script
-cd /darp/scripts
+#echo `date` Launching forever script
+#cd /darp/scripts
 #./forever.bash  #Start the system
 
