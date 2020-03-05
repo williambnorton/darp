1fa746b04d4eda0ccd652195ff84ee6debde6cee
William B. Norton <wbn@drpeering.net>
2020-03-04 16:21:12 -0800

    ./builddarp.bash working on CI

--- builddarp.bash
+++ builddarp.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo `date` compiling typescript into javascript 
+rm build.??????.????
 date>"build."`date +%y%m%d.%H%M`
 tsc config/config && tsc express/express && tsc pulser/pulser && tsc handlepulse/handlepulse && git add . && git commit -m "$0 working on CI" && git push
 echo `date` Completed compiles and git push
