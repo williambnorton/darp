120847f3ae232cd3daff7459b948c346a09e5b21
William B. Norton <wbn@drpeering.net>
2020-03-06 21:17:26 -0800

    working on config and express and scripts

--- builddarp.bash
+++ builddarp.bash
@@ -3,6 +3,6 @@ echo `date` compiling typescript into javascript
 rm build.??????.????
 date>"build."`date +%y%m%d.%H%M`
 ls -l "build."`date +%y%m%d.%H%M`
-tsc config/config && tsc express/express && tsc pulser/pulser && tsc handlepulse/handlepulse && git add . && git commit -m "$0 working on CI" && git push
+tsc config/config && tsc express/express && tsc pulser/pulser && tsc handlepulse/handlepulse && git add . && git commit -m "working on config and express and scripts" && git push
 
 echo `date` Completed compiles and git push
