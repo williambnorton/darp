42b37a0864b9857f7a2a58da93257dd1de2b85be
William B. Norton <wbn@drpeering.net>
2020-03-09 14:41:12 -0700

    working on config and express and scripts

--- bootdarp.bash
+++ bootdarp.bash
@@ -2,34 +2,24 @@
 #		    bootdarp.bash - fetch updated darp software and launch forever script
 #
 
-#this should be a check to see if code has changed and the do soft reload.
-# for now, just replace code with new
-cd /tmp
-rm -rf /tmp/darp
-mv /darp /tmp/darp
-echo `date` Cloning new darp code from github
-git clone https://github.com/williambnorton/darp.git    /darp     
-
+/darp/updateWG.bash
+echo `date` bootdarp: SOFTWARE UPDATE COMPLETE
 #Now we are running in the new code /darp directory
 cd /darp
 ls -l /darp
 
-echo `date` configuring initial wireguard keys
+echo `date` bootdarp: configuring initial wireguard keys
 cd /darp/scripts/
 ./configWG.bash
 PUBLICKEY=`cat /etc/wireguard/publickey`
 echo PUBLICKEY=$PUBLICKEY
 cd /darp
 redis-server &  #temp to help building
-
+sleep 1
 cd /darp/express
 node express &
+sleep 1
 
-cd /darp/handlepulse
-node handlepulse &
-
-echo    `date` Waiting for express to start web services for nodefactory service 
-sleep 5
 
 #echo `date` Launching forever script
 #cd /darp/scripts
@@ -39,7 +29,12 @@ cd /darp/config
 node config &
 
 echo    `date` Waiting for config to connect
-sleep 5
+sleep 3
+
+cd /darp/handlepulse
+node handlepulse &
+echo    `date` Waiting for express to start web services for nodefactory service 
+sleep 3
 
 echo `date` New darp version: `cd /darp;ls build*` installed and running
 
---
+++ updateSW.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+#		    bootdarp.bash - fetch updated darp software and launch forever script
+#
+echo `date` compiling typescript into javascript 
+#this should be a check to see if code has changed and the do soft reload.
+# for now, just replace code with new
+cd /tmp
+rm -rf /tmp/darp
+mv /darp /tmp/darp
+echo `date` Cloning new darp code from github
+git clone https://github.com/williambnorton/darp.git    /darp     
+
+echo `date` Completed git clone for `ls build*`
