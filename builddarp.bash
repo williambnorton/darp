ec85d11a8ae76e470d0c5f5b7282d84cdc269da3
William B. Norton <wbn@drpeering.net>
2020-03-09 07:54:36 -0700

    working on config and express and scripts

--- builddarp.bash
+++ builddarp.bash
@@ -5,4 +5,4 @@ date>"build."`date +%y%m%d.%H%M`
 ls -l "build."`date +%y%m%d.%H%M`
 tsc config/config && tsc express/express && tsc pulser/pulser && tsc handlepulse/handlepulse && git add . && git commit -m "working on config and express and scripts" && git push
 
-echo `date` Completed compiles and git push
+echo `date` Completed compiles and git push for `cd /darp;ls build*`
