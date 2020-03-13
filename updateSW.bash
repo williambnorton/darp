6878151039beb7993c6e69fb8b6f3ee7c02079a5
William B. Norton <wbn@drpeering.net>
2020-03-13 06:31:33 -0700

    working on config and express and scripts

--- updateSW.bash
+++ updateSW.bash
@@ -6,8 +6,8 @@ echo `date` updateSW.bash
 # for now, just replace code with new
 cd /tmp
 rm -rf /tmp/darp
-mv /darp /tmp/darp
+mv ~/darp /tmp/darp
 echo `date` Cloning new darp code from github
-git clone https://github.com/williambnorton/darp.git    /darp     
-cd /darp
-echo `date` Completed git clone for `ls build*`
+git clone https://github.com/williambnorton/darp.git    ~/darp     
+cd ~/darp
+echo `date` Completed git clone into ~/darp for `ls build*`
