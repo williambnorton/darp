41995a1e5f2f4891e6576fc5385941dc9dbdd9db
William B. Norton <wbn@drpeering.net>
2020-03-26 17:13:38 -0700

    working on config and express and scripts

--- bootdarp.bash
+++ bootdarp.bash
@@ -1,70 +1,79 @@
 #!/bin/bash
 #		    bootdarp.bash - fetch updated darp software and launch forever script
 # 
-DARPDIR=~/darp
-
+DARPDIR=$HOME/darp
+#If the GENESIS variable ENV VAR does not exist then assume we are genesis node
 if [ "$GENESIS" == "" ]; then
    GENESIS=`curl ifconfig.co`
-    echo `date` GENESIS set to $GENESIS
+   echo `date` GENESIS set to $GENESIS
 fi
 
 #update SW is destructive - should be done after run in docker loop
 #when genesis node leanrs of new SW it quits and downloads 
 #
 #The order of startup is important here
+while :
+    echo `date` $0 : kill old processes to be restarted
+    kill `cat $DARPDIR/*.pid`
+    sleep 1
+    ./updateSW.bash
+    echo `date` SOFTWARE UPDATE COMPLETE
+    cd $DARPDIR
+    echo `date` RUNNING SOFTWARE VERSION `ls -l *build*`
 
-./updateSW.bash
-echo `date` bootdarp: SOFTWARE UPDATE COMPLETE
+    #Now we are running in the new code /darp directory
+    echo `date` Configuring initial wireguard keys
+    cd $DARPDIR/scripts/
+    ./configWG.bash
+    export PUBLICKEY=`cat $DARPDIR/wireguard/publickey`
+    echo PUBLICKEY=$PUBLICKEY
+    sleep 1
 
-#Now we are running in the new code /darp directory
-cd ~
-cd $DARPDIR
-ls -l 
+    cd $DARPDIR
+    echo `date` Starting redis
+    redis-cli shutdown  #stop server
+    redis-server &  #temp to help building
+    echo $$>$DARPDIR/redis-server.pid
+    echo `date`" redis started"
+    sleep 1
 
-echo `date` bootdarp: configuring initial wireguard keys
-cd $DARPDIR/scripts/
-./configWG.bash
-export PUBLICKEY=`cat $DARPDIR/wireguard/publickey`
-echo PUBLICKEY=$PUBLICKEY
-cd $DARPDIR
+    #
+    #   need express (TCP/65013) before config
+    #
+    cd $DARPDIR
+    echo `date` Starting express for nodeFactory and externalize stats
+    cd $DARPDIR/express
+    node express &
+    echo $$>$DARPDIR/express.pid
+    sleep 1
 
-echo `date` bootdarp: starting redis
-redis-cli shutdown  #stop server
-redis-server &  #temp to help building
-echo $$>$DARPDIR/redis-server.pid
-sleep 1
-#
-#   need express (TCP/65013) before config
-#
-echo `date` bootdarp: starting express
-cd ~/darp/express
-node express &
-echo $$>$DARPDIR/express.pid
-sleep 2
+    #echo `date` Launching forever script
+    #cd /darp/scripts
+    #./forever.bash  #Start the system
+    cd $DARPDIR
+    echo `date` Connecting to GENESIS node to get configuration into redis
+    cd $DARPDIR/config
+    kill `cat $DARPDIR/config.pid`
+    node config &
+    echo $$>$DARPDIR/config.pid
+    echo `date` Waiting for config to connect
+    sleep 1
 
-#echo `date` Launching forever script
-#cd /darp/scripts
-#./forever.bash  #Start the system
+    cd $DARPDIR
+    cd $DARPDIR/pulser
+    kill `cat pulser.pid`
+    node pulser &
+    echo $$>$DARPDIR/pulser.pid
+    #echo `date` '------------> Please start pulser'
 
-cd ~/darp/config
-kill `cat config.pid`
-node config &
-echo $$>$DARPDIR/config.pid
-echo `date` Waiting for config to connect
-sleep 1
+    cd $DARPDIR
+    cd $DARPDIR/handlepulse
+    kill `cat handlepulse.pid`
+    node handlepulse 
+    #echo $$>$DARPDIR/handlepulse.pid
+    #echo `date` Starting handlepulse
+    echo `handlePulse finished -restarting all`
+    sleep 10 
+    echo `date` New darp version: `cd /darp;ls build*` installed and running
 
-echo `date` New darp version: `cd /darp;ls build*` installed and running
-
-cd ~/darp/handlepulse
-kill `cat handlepulse.pid`
-node handlepulse &
-echo $$>$DARPDIR/handlepulse.pid
-echo `date` Starting handlepulse
-sleep 1
-
-cd ~/darp/pulser
-kill `cat pulser.pid`
-node pulser &
-echo $$>$DARPDIR/pulser.pid
-#echo `date` '------------> Please start pulser'
-
+done
