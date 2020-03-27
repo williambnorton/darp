bb5ffb4847bc63d932057f812db4bf69f8b87dff
William B. Norton <wbn@drpeering.net>
2020-03-27 12:23:37 -0700

    working on config and express and scripts

--- builddarp.bash
+++ builddarp.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo `date` compiling typescript into javascript 
-rm build.??????.????
+rm Build.??????.????
 find . -name '*.pid' -delete
 
-date>"build."`date +%y%m%d.%H%M`
-ls -l "build."`date +%y%m%d.%H%M`
+date>"Build."`date +%y%m%d.%H%M`
+ls -l "Build."`date +%y%m%d.%H%M`
 npm i @types/node
 tsc config/config && tsc express/express && tsc pulser/pulser && tsc handlepulse/handlepulse && git add . && git commit -m "working on config and express and scripts" && git push
 
-echo `date` Completed compiles and git push for `ls build*`
+echo `date` Completed compiles and git push for `ls Build*`
