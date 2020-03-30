c36181d9763456959a28eaf8246bdfe89e22e5da
William B. Norton <wbn@drpeering.net>
2020-03-29 17:01:05 -0700

    stabliizing base platform for launch

--- updateSW.bash
+++ updateSW.bash
@@ -1,15 +1,10 @@
 #!/bin/bash
 #		    updateSW.bash
 #
-echo parm count= $#
 if [ $# -ne 0 ]; then
     echo `date` Running $0 in DAEMON mode. $#
     DAEMON="YES"
 fi
-    cd $DARPDIR
-    npm update
-    npm i @types/node
-    npm install redis express
 
 echo `date` updateSW.bash
 while :
@@ -21,11 +16,16 @@ do
     echo `date` Current SW is `ls Build*`
     cd /tmp
     rm -rf /tmp/darp
-    mv $DARPDIR /tmp/darp
+    #mv $DARPDIR /tmp/darp
     echo `date` Cloning new darp code from github
     git clone https://github.com/williambnorton/darp.git    /tmp/darp     
     cd /tmp/darp
     NEW=`ls Build*`
+
+    cd $DARPDIR
+    npm update
+    npm i @types/node
+    npm install redis express
 #
 #   We are in the moved old DARP directory (if things changed)
 #
@@ -39,6 +39,7 @@ do
         fi
     else
 	    echo `date` Software changed. Was $CURRENT Now is $NEW
+        
         echo 'CLONED INTO new darp directory.      YOU NEED A new Bash shell:      cd ~;cd darp;ls'
         echo Should kill handlepulse to force reload
         kill `ps aux | grep 'node handlepulse'| grep -v grep | awk '{ print $2}'`
