c97775cb87f02688e9648b494a62c812a14acaa4
William B. Norton <wbn@drpeering.net>
2020-03-31 18:43:39 -0700

    stabliizing base platform for launch

--- express/express.ts
+++ express/express.ts
@@ -167,6 +167,9 @@ app.get('/nodefactory', function (req, res) {
             };
             gSRlist=","+geo+":"+genesis.geo+".1";
          }
+         expressRedisClient.hmset( "gSRlist", newMint, geo+":"+genesis.geo+".1" );
+         expressRedisClient.hmset( "gSRlist", 1, genesis.geo+":"+genesis.geo+".1" );
+
          var node={
             mint0 : newMintRecord,     //YOU
             mint1 : genesis,           //GENESIS NODE
