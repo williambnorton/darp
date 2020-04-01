4c36101625bc65a9a3f36614ab2692ee0c3f1d15
William B. Norton <wbn@drpeering.net>
2020-04-01 13:04:33 -0700

    stabliizing base platform for launch

--- updateSW.bash
+++ updateSW.bash
@@ -33,9 +33,11 @@ do
         echo 'CLONED INTO /tmp directory.      YOU NEED A new Bash shell:      cd ~;cd darp;ls'
         echo Killing handlepulse to force reload: `ls $DARPDIR/*.pid`
 
-        killList=`cat $DARPDIR/*.pid`
+
+        #killList=`cat $DARPDIR/*.pid`
+        killlist=`ps aux|grep "node handlepulse" | grep -v grep | awk '{ print $2}'`
         if [ "$killList" != "" ]; then
-            echo `date` killList=$killList
+            echo `date` Killing handlePulse to start software reload. Killing pid: $killList
             kill $killList
         fi
         cd /tmp
