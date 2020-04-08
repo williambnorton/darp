1f30a6d10cb73cafee7c64a4f0bcc9f6aa9330be
William B. Norton <wbn@drpeering.net>
2020-04-08 14:00:23 -0700

    Simulator stable Redis-based nodes + stabliizing base platform for launch

--- builddarp.bash
+++ builddarp.bash
@@ -2,10 +2,10 @@
 echo `date` compiling typescript into javascript 
 rm Build.??????.????
 find . -name '*.pid' -delete
-
+MESSAGE="Simulator stable Redis-based nodes"
 date>"Build."`date +%y%m%d.%H%M`
 ls -l "Build."`date +%y%m%d.%H%M`
 
-tsc config/config && tsc express/express && tsc pulser/pulser && tsc handlepulse/handlepulse && git add . && git commit -m "stabliizing base platform for launch" && git push
+tsc config/config && tsc express/express && tsc pulser/pulser && tsc handlepulse/handlepulse && git add . && git commit -m "$MESSAGE + stabliizing base platform for launch" && git push
 
 echo `date` Completed compiles and git push for `ls Build*`
