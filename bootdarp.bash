ab4b623ee4f34c71841b42e4278ec24dc234ad4d
William B. Norton <wbn@drpeering.net>
2020-04-10 18:47:34 -0700

    Simulator stable Redis-based nodes + stabliizing base platform for launch

--- bootdarp.bash
+++ bootdarp.bash
@@ -60,7 +60,7 @@ do
         echo `date` "I AM GENESIS NODE"
         echo `date` "I AM GENESIS NODE"
         echo `date` "I AM GENESIS NODE"
-        echo `date` "I AM GENESIS NODE"
+        echo `date` "I AM GENESIS NODE - Starting up auto-updater"
         sleep 1
         ( ./updateSW.bash -deamon 2>&1 ) & ####>/dev/null & #keep it checking every 30 seconds
     fi
