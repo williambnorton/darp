8bb35aacf70799dbccea6d5f00b717207b89053e
William B. Norton <wbn@drpeering.net>
2020-04-15 14:32:28 -0700

    Simulator stable Redis-based nodes + stabliizing base platform for launch

--- bootdarp.bash
+++ bootdarp.bash
@@ -165,15 +165,15 @@ do
   *)
     echo `date` $0 rc=$rc ... updateSW.bash detected NEW SOFTWARE and killed handlepulse processes
     echo `date` $0 result: unexpected rc out of handlepulse rc=$rc
-    //if [ "$GENESISIP" = "$MYIP" ]; then
+    #//if [ "$GENESISIP" = "$MYIP" ]; then
         echo `date` "  Ctrl-C         Ctrl-C           Ctrl-C         Ctrl-C "
         echo `date` "  Ctrl-C         Ctrl-C           Ctrl-C         Ctrl-C "
         echo `date` "  Ctrl-C         Ctrl-C           Ctrl-C         Ctrl-C "
         echo `date` "  Ctrl-C         Ctrl-C           Ctrl-C         Ctrl-C "
         echo `date` "  Ctrl-C         Ctrl-C           Ctrl-C         Ctrl-C "
         exit -1
-    //fi
-    //echo `date` Ctrl-C detected --OR-- Genesis node needs updated code
+    #//fi
+    #//echo `date` Ctrl-C detected --OR-- Genesis node needs updated code
     
     ;;
 esac
