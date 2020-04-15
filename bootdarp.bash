c21c93a791a7be3a7518c9a0edff23e8df6ffe86
William B. Norton <wbn@drpeering.net>
2020-04-15 14:34:03 -0700

    Simulator stable Redis-based nodes + stabliizing base platform for launch

--- bootdarp.bash
+++ bootdarp.bash
@@ -177,11 +177,12 @@ do
     
     ;;
 esac
-echo `date` "...................BOTTOM OF LOOP..................." 
 echo `date` killing `ps aux |grep -v grep | grep node | awk '{ print $2}'`
 kill -9 `ps aux |grep -v grep | grep node | awk '{ print $2}'`
 kill -9 `ps aux |grep -v grep | grep updateSW.bash | awk '{ print $2}'`
 
 ps aux
+echo `date` "...................BOTTOM OF LOOP................... SLEEPING" 
+
 sleep 5
 done
