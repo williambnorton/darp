0eb436debbeead750e7cadb8ad50236105e790f9
William B. Norton <wbn@drpeering.net>
2020-04-16 12:08:55 -0700

    Simulator stable Redis-based nodes + stabliizing base platform for launch

--- pulser/pulser.js
+++ pulser/pulser.js
@@ -26,8 +26,8 @@ function pulse() {
     setTimeout(pulse, 10 * 1000); //10 second pollingfrequency
     //  get all my pulseGroups
     redisClient.hgetall("mint:0", function (err, me) {
-        if (me == null || me.state == "PAUSE")
-            return console.log("(nomint)PAUSED ");
+        if (me == null || me.state == "HOLD")
+            return console.log("no mint:0 or HOLD ");
         GEO = me.geo;
         var cursor = '0'; // DEVOPS:* returns all of my pulseGroups
         redisClient.scan(cursor, 'MATCH', me.geo + ":*", 'COUNT', '100', function (err, pulseGroups) {
