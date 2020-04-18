9b61d5c93e818eccceffe2a99a89d33b7bb62b38
William B. Norton <wbn@drpeering.net>
2020-04-18 11:11:50 -0700

    Delivers OWL matrix via pub/sub + stabliizing base platform for launch

--- bootdarp.bash
+++ bootdarp.bash
@@ -13,7 +13,11 @@
 #           GENESIS - if it isn't passed in , we find one from DrPeering
 #           DARPDIR - the root of all darp info
 #
-
+echo `date` "-------------------------------- $0 STARTING ----------------------------------" 
+echo `date` "-------------------------------- $0 STARTING ----------------------------------" 
+echo `date` "-------------------------------- $0 STARTING ----------------------------------" 
+echo `date` "-------------------------------- $0 STARTING ----------------------------------" 
+echo `date` "-------------------------------- $0 STARTING ----------------------------------" 
 export DARPDIR=$HOME/darp
 MYIP=`curl ifconfig.io`
 export MYIP=$MYIP
