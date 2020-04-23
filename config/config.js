548ec7c4a19d1ce0304ad10bf14bd1afd8b0b70f
William B. Norton <wbn@drpeering.net>
2020-04-23 10:55:38 -0700

    Delivers OWL matrix via pub/sub + stabliizing base platform for launch

--- config/config.js
+++ config/config.js
@@ -74,7 +74,7 @@ redisClient.hmset("mint:0",{
     "owl": ""   //how long it took this node's last record to reach me
  });
 */
-redisClient.hmgetall("mint:0", function (err, me) {
+redisClient.hgetall("mint:0", function (err, me) {
     var PUBLICKEY = me.publickey;
 }); //we need this to authenticate self as genesis
 getConfiguration(); //later this should start with just an IP of genesis node 
