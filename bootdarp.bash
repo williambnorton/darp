24665b71aaa3d72cd83abe3de85324807ef54813
William B. Norton <wbn@drpeering.net>
2020-04-23 17:52:20 -0700

    Delivers OWL matrix via pub/sub + stabliizing base platform for launch

--- bootdarp.bash
+++ bootdarp.bash
@@ -20,7 +20,9 @@ echo `date` "------------------ $0 STARTING --------------------"
 echo `date` "------------------ $0 STARTING --------------------" 
 
 export DARPDIR=$HOME/darp
-if ["$PORT" = "" ]; then; PORT=65013; fi;
+if ["$PORT" = "" ]; then 
+    PORT=65013; 
+fi
 export PORT
 echo PORT=$PORT
 
