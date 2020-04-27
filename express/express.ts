416b317b32aa6ea4fb72cc10c3c6793ef1a1b859
William B. Norton <wbn@drpeering.net>
2020-04-27 13:28:50 -0700

    Delivers OWL matrix via pub/sub + stabliizing base platform for launch

--- express/express.ts
+++ express/express.ts
@@ -274,7 +274,7 @@ function getMintTable(callback) {
                   txt+="<td>"+mintEntry.port+"</td>"
                   txt+="<td>"+mintEntry.ipaddr+"</td>"
                   txt+="<td>"+mintEntry.publickey.substring(0,3)+"..."+mintEntry.publickey.substring(40,mintEntry.publickey.length)+"</td>"
-                  txt+="<td>"+'<a href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/config" target="_blank">' + mintEntry.state + '</a>'+"</td>"
+                  txt+="<td>"+'<a href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/config" >' + mintEntry.state + '</a>'+"</td>"
                   txt+="<td>"+mintEntry.bootTime+"</td>"
                   txt+="<td>"+mintEntry.version+"</td>"
                   txt+="<td>"+mintEntry.wallet.substring(0,3)+"..."+mintEntry.wallet.substring(40,mintEntry.wallet.length)+"</td>"
@@ -350,6 +350,72 @@ function handleShowState(req, res) {
 
       display(function (html) {
          makeConfigAll(function(config) {
+            console.log(ts()+"config="+dump(config));
+            /*
+            console.log(ts()+"config.mintTable="+dump(config.mintTable));
+            var txt='<br><h2>mintTable</h2><table border="1">';
+            txt+="<tr>"
+            txt+="<th>mint</th>"
+            txt+="<th>geo</th>"
+            txt+="<th>port</th>"
+            txt+="<th>ipaddr</th>"
+            txt+="<th>publickey</th>"
+            txt+="<th>state</th>"
+            txt+="<th>bootTime</th>"
+            txt+="<th>version</th>"
+            txt+="<th>wallet</th>"
+            txt+="<th>SHOWPULSES</th>"
+            txt+="<th>owl</th>"
+            txt+="<th>isGenesisNode</th>"
+            txt+="<th>clockSkew</th>"
+            txt+="<th>CONTROLS</th>"
+
+            txt+="</tr>"
+            var mintTable=config.mintTable
+
+            for (var mintEntry in mintTable) {
+               console.log(ts()+"mintEntry="+mintEntry);
+
+               txt+="<tr>"
+               txt+="<td>"+mintEntry.mint+"</td>"
+               txt+="<td>"+mintEntry.geo+"</td>"
+               txt+="<td>"+mintEntry.port+"</td>"
+               txt+="<td>"+mintEntry.ipaddr+"</td>"
+               txt+="<td>"+mintEntry.publickey.substring(0,3)+"..."+mintEntry.publickey.substring(40,mintEntry.publickey.length)+"</td>"
+               txt+="<td>"+'<a href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/config" >' + mintEntry.state + '</a>'+"</td>"
+               txt+="<td>"+mintEntry.bootTime+"</td>"
+               txt+="<td>"+mintEntry.version+"</td>"
+               txt+="<td>"+mintEntry.wallet.substring(0,3)+"..."+mintEntry.wallet.substring(40,mintEntry.wallet.length)+"</td>"
+               txt+="<td>"+mintEntry.SHOWPULSES+"</td>"
+               txt+="<td>"+mintEntry.owl+"</td>"
+               txt+="<td>"+mintEntry.isGenesisNode+"</td>"
+               txt+="<td>"+mintEntry.clockSkew+"</td>"
+      
+               var stopButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/stop";
+               var rebootButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/reboot";
+               var reloadButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/reload";
+               var SINGLESTEPButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/SINGLESTEP";
+               var pulseMsgButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/pulseMsg";
+      
+               txt += "<td>" + '<FORM>';
+               txt += '<INPUT Type="BUTTON" Value="PULSE1" Onclick="window.location.href=\'' + pulseMsgButtonURL + "'" + '">';
+               txt += '<INPUT Type="BUTTON" Value="RELOAD" Onclick="window.location.href=\'' + reloadButtonURL + "'" + '">';
+               txt += '<INPUT Type="BUTTON" Value="SINGLESTEP" Onclick="window.location.href=\'' + SINGLESTEPButtonURL + "'" + '">';
+               txt += '<INPUT Type="BUTTON" Value="STOP" Onclick="window.location.href=\'' + stopButtonURL + "'" + '">';
+               txt += '<INPUT Type="BUTTON" Value="REBOOT" Onclick="window.location.href=\'' + rebootButtonURL + "'" + '">';
+               txt += '</FORM>' + "</td>";
+      
+               txt+="</tr>"
+            }
+            txt+="</table>"; 
+
+
+*/
+
+
+
+
+
             //console.log("app.get('/state' callback config="+dump(config));
             //expressRedisClient.hgetall("mint:0", function(err, me) {
             //   config.mintTable["mint:0"]=me;
