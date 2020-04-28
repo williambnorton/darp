aea177de849b82ee55926f0aec8e2886cb10c8a7
William B. Norton <wbn@drpeering.net>
2020-04-28 12:47:18 -0700

    Delivers OWL matrix via pub/sub + stabliizing base platform for launch

--- express/express.js
+++ express/express.js
@@ -21,8 +21,8 @@ var express = require('express');
 var app = express();
 var mintStack = 1;
 var DEFAULT_SHOWPULSES = "0";
-var DEFAULT_START_STATE = "SINGLESTEP"; //for single stepping through network protocol code
-//const DEFAULT_START_STATE="RUNNING";  //for single stepping through network protocol code
+//const DEFAULT_START_STATE="SINGLESTEP";  //for single stepping through network protocol code
+var DEFAULT_START_STATE = "RUNNING"; //for single stepping through network protocol code
 //const DEFAULT_START_STATE="RUNNING"; console.log(ts()+"EXPRESS: ALL NODES START IN RUNNING Mode");
 //const DEFAULT_START_STATE="SINGLESTEP"; console.log(ts()+"EXPRESS: ALL NODES START IN SINGLESTEP (no pulsing) Mode");
 /****  NODE SITE CONFIGURATION  ****/
