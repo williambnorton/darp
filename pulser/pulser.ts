5f6a2cace1d2016ee9eb7b2b29417b41a127614c
William B. Norton <wbn@drpeering.net>
2020-04-29 14:10:30 -0700

    Delivers OWL matrix via pub/sub + stabliizing base platform for launch

--- pulser/pulser.ts
+++ pulser/pulser.ts
@@ -329,7 +329,7 @@ function buildPulsePkt(mints, pulseMsg, sendToAry) {
                   "statsPulseMessageLength" : ""+pulseMsg.length
                 });
                 //sending msg
-                //console.log("networkClient.send(pulseMsg="+pulseMsg+" node.port="+node.port+" node.ipaddr="+node.ipaddr);
+                console.log("networkClient.send(pulseMsg="+pulseMsg+" node.port="+node.port+" node.ipaddr="+node.ipaddr);
 
                 networkClient.send(pulseMsg,node.port,node.ipaddr,function(error){     //*** send Message
                 
