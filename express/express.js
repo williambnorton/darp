8ca1d53e2a4942e8aefff03c33ce73df424b202a
William B. Norton <wbn@drpeering.net>
2020-04-30 17:02:10 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.js
+++ express/express.js
@@ -141,7 +141,7 @@ function getMatrixTable(darpMatrix, callback) {
         for (var n in reply[1]) {
             var ary = reply[1][n].split("-"); ///"darp-1-3=35",
             var src = ary[1], dst = ary[2], owl = ary[3];
-            console.log(lib_1.ts() + "src=" + src + " dst=" + dst + "=" + owl);
+            console.log(lib_1.ts() + "getMatrixTable src=" + src + " dst=" + dst + " owl=" + owl);
             if (typeof darpMatrix[src] == "undefined")
                 darpMatrix[src] = {}; //new Array();
             if (typeof darpMatrix[src][dst] == "undefined")
--- express/express.ts
+++ express/express.ts
@@ -153,7 +153,7 @@ function getMatrixTable(darpMatrix,callback) {
     for (var n in reply[1]) {
       var ary=reply[1][n].split("-")   ///"darp-1-3=35",
       var src=ary[1], dst=ary[2], owl=ary[3];
-      console.log(ts()+"src="+src+" dst="+dst+"="+owl);
+      console.log(ts()+"getMatrixTable src="+src+" dst="+dst+" owl="+owl);
       if (typeof darpMatrix[src] == "undefined" ) darpMatrix[src]={} //new Array();
       if (typeof darpMatrix[src][dst] == "undefined" ) darpMatrix[src][dst]={} //new Array();
       //console.log(ts()+"Storing darpMatrix["+src+"]["+dst+"]="+owl);  ///INSTRUMENTATION POINT
