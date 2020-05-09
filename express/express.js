d62ebf7ec461eb9f8520c80a0353e1945dd266fb
William B. Norton <wbn@drpeering.net>
2020-05-09 15:49:34 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.js
+++ express/express.js
@@ -276,7 +276,7 @@ function handleShowState(req, res) {
                     //getIPport(rowEntry.srcMint,function (IPnPort) {  //experiment
                     //txt += '<tr><td>' + IPnPort + '</td>'; //heacer on left side
                     var cellState = "RUNNING"; //unreachable     badkey   alert   
-                    txt += '<tr><td><a href="http://' + me.ipaddr + ":" + me.port + '/">' + rowEntry.geo + " " + rowEntry.srcMint + '</a></td>'; //heacer on left side
+                    txt += '<tr><td><a href="http://' + rowEntry.ipaddr + ":" + rowEntry.port + '/">' + rowEntry.geo + " " + rowEntry.srcMint + '</a></td>'; //heacer on left side
                     for (var col in pulses) {
                         var colEntry = pulses[col]; //
                         var entryLabel = rowEntry.geo + "-" + colEntry.geo;
--- express/express.ts
+++ express/express.ts
@@ -297,7 +297,7 @@ function handleShowState(req, res) {
 
                    //txt += '<tr><td>' + IPnPort + '</td>'; //heacer on left side
                     var cellState="RUNNING"; //unreachable     badkey   alert   
-                   txt += '<tr><td><a href="http://' + me.ipaddr+":"+me.port+'/">'+rowEntry.geo + " " + rowEntry.srcMint + '</a></td>'; //heacer on left side
+                   txt += '<tr><td><a href="http://' + rowEntry.ipaddr+":"+rowEntry.port+'/">'+rowEntry.geo + " " + rowEntry.srcMint + '</a></td>'; //heacer on left side
                    for (var col in pulses) {
                        var colEntry = pulses[col];//
                        var entryLabel = rowEntry.geo + "-" + colEntry.geo
