d6a96d879192844510d859f72ab6bab9a9d40eaf
William B. Norton <wbn@drpeering.net>
2020-05-10 16:44:31 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.js
+++ express/express.js
@@ -229,7 +229,7 @@ function handleShowState(req, res) {
         txt += '  $("#"+pulse.geo+"_owl").html(pulse.owl);';
         txt += '  $("#"+pulse.geo+"_owls").html(pulse.owls);';
         //       txt +=    '  $("#"+pulse.geo+"-"+"'+me.geo+'").html(pulse.owl);'  //color matrix value directly measured
-        txt += '  $("#"+pulse.geo+"-"+"' + me.geo + '").text(pulse.owl);'; //color matrix value directly measured
+        txt += '  $("#"+pulse.geo+"-"+"' + me.geo + '").html("<a href=\"http://\">"+pulse.owl+"</a>");'; //color matrix value directly measured
         /***/
         //txt +=    '  for (pulse.owls);'
         txt += "}";
