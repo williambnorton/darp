0a98b09cfba763937fee2150017b4be18a9f8153
William B. Norton <wbn@drpeering.net>
2020-05-11 07:54:41 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.ts
+++ express/express.ts
@@ -226,8 +226,9 @@ function handleShowState(req, res) {
 
        txt += 'for (let [key, value] of Object.entries(config.pulses)) {'
        txt += 'var node=${key}.split(":")[0]'
+       txt += 'for (let [key, value] of Object.entries(config.pulses[${key}])) {'
        txt += ' console.log(`COULD GENERALIZE: ${key}.split(":")[0]: ${value} ---> $("."+pulse.geo+"_"+${key}+").html("+${value}+");`);'
-       txt += '}'
+       txt += '}}'
 
 
        txt +=    "for (var node in config.pulses) { "  //here we could generalize - power tool - for each field, set
