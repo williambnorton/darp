b8d7136fc0ec48c8a63510e3f5bdad6fc9d3913e
William B. Norton <wbn@drpeering.net>
2020-05-11 20:44:21 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.ts
+++ express/express.ts
@@ -218,12 +218,13 @@ function handleShowState(req, res) {
        txt += '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>'
        txt += ' <script type = "text/javascript" language="javascript">'
        txt += " var URL='http://"+me.ipaddr+":"+me.port+"/state';"
+/*
        txt += 'function fetchState() {'
        txt += '   $.getJSON(URL, function(config) {'
        txt += "      var d = new Date(); var now=d.getTime();"
        txt += '      $("#dateTime").html( "*"+d.toString()+"*" );'
 
-       /**
+    
        txt += '      for (let [key, value] of Object.entries(config.pulses)) {'
        //                txt += '   console.log(`FOR EACH PULSE  ${key}.split(":")[0]: ${value} ---> $("."+pulse.geo+"_"+${key}+").html("+${value}+");`);'
        txt += '          var pulseLabel=key;'
@@ -261,13 +262,13 @@ function handleShowState(req, res) {
         //txt += '         }'
   
         txt += '       }'
-        ***/
+        
         txt += "    });"
         txt += "    setTimeout(fetchState,5000);"
 
         txt += "}"
         txt += "setTimeout(fetchState,1000);"
-
+*/
         txt += '</script> '
         txt += '</head>'
 
