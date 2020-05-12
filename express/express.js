1182b4f890c6340b7aa7237f4bafc1f0072c341c
William B. Norton <wbn@drpeering.net>
2020-05-12 09:31:39 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.js
+++ express/express.js
@@ -221,6 +221,7 @@ function handleShowState(req, res) {
         //        txt += '             $("."+pulse.geo+"_"+field).html( aRecord );'
         //                       txt += '<td class="' + rowEntry.srcMint + "-" + colEntry.srcMint+'">' + '<a  target="_blank" href="http://' + me.ipaddr + ':' + me.port + '/graph?src=' +  rowEntry.geo+'&dst='+colEntry.geo +  "&group=" + me.group + '" >' + owl + "ms</a>" + " ms</td>"
         txt += 'var href="<a href=\"" + "http://" + "' + me.ipaddr + ':' + me.port + '/graph?srcMint=" + rowEntry.srcMint + "&dstMint=" + colEntry.srcMint + "&group=" + me.group + "\">" + pulse[field] + "</a>" ; ';
+        txt += 'console.log("href="+href);';
         //txt += '             $("."+pulse.geo+"_"+field).html( \'<a href=\"http://' + me.ipaddr + ':' + me.port + '/graph?srcMint=" +  rowEntry.srcMint + "&dstMint=" + colEntry.srcMint +  "&group=" + me.group + "> *" + pulse[field] + "*</a>\'+" );'
         //txt += '             $("."+pulse.geo+"_"+field).html(\'<a href=" + href + '">"' + '+pulse[field] + "</a>" );'
         txt += '         }';
