9b14e3ffb7c293469e3f69291d8a37ed1a5010c2
William B. Norton <wbn@drpeering.net>
2020-05-13 15:39:22 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.js
+++ express/express.js
@@ -233,7 +233,7 @@ function handleShowState(req, res) {
         //        txt += '              var link=\'<a href="http://'+me.ipaddr+':'+me.port+'">\'+owl+" ms </a>";'
         txt += '              var link=\'<a target="_blank" href="http://' + me.ipaddr + ':' + me.port + '/graph?srcMint=\';';
         txt += '              link += srcMint + "&dstMint=" + dstMint + "&group=" + "' + me.group + '"+ \'">\' + owl + " ms *</a>";';
-        txt += '              console.log("my link="+link);';
+        //        txt += '              console.log("my link="+link);'
         txt += '              $("."+srcMint+"-"+dstMint).html(link);';
         txt += '          }';
         txt += '       }';
