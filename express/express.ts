81db8779d16271c3a61adc37c525eaf841687238
William B. Norton <wbn@drpeering.net>
2020-05-13 14:23:41 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.ts
+++ express/express.ts
@@ -428,22 +428,13 @@ function handleShowState(req, res) {
                    txt += '<td class="'+pulseEntry.geo+'_outOctets"'+'>' + pulseEntry.outOctets + "</td>"
                    txt += '<td class="'+pulseEntry.geo+'_inMsgs"'+'>' + pulseEntry.inMsgs + "</td>"
                    txt += '<td class="'+pulseEntry.geo+'_outMsgs"'+'>' + pulseEntry.outMsgs + "</td>"
-                   var pktLoss=parseInt(pulseEntry.seq)-parseInt(pulseEntry.inMsgs);
-                   console.log("pktloss=:"+pktLoss);
-                   console.log("pktloss=:"+pktLoss);
-                   console.log("pktloss=:"+pktLoss);
-                   console.log("pktloss=:"+pktLoss);
-                   console.log("pktloss=:"+pktLoss);
-                   console.log("pktloss=:"+pktLoss);
-                   console.log("pktloss=:"+pktLoss);
-                   console.log("pktloss=:"+pktLoss);
-                   console.log("pktloss=:"+pktLoss);
-                   console.log("pktloss=:"+pktLoss);
-                   console.log("pktloss=:"+pktLoss);
-                   if (pktLoss>1) 
-                        txt += '<td class="'+pulseEntry.geo+'_pktDrops BAD"'+'>' + pktLoss + "</td>";
+                   //var pktLoss=parseInt(pulseEntry.seq)-parseInt(pulseEntry.inMsgs);
+                   //console.log("pktloss=:"+pktLoss);
+                
+                   if (pulseEntry.pktDrops>1) 
+                        txt += '<td class="'+pulseEntry.geo+'_pktDrops BAD"'+'>' + pulseEntry.pktDrops + "</td>";
                     else 
-                        txt += '<td class="'+pulseEntry.geo+'_pktDrops GOOD"'+'>' + pktLoss + "</td>";
+                        txt += '<td class="'+pulseEntry.geo+'_pktDrops GOOD"'+'>' + pulseEntry.pktDrops + "</td>";
                    if (pulseEntry.lastMsg) {
                         txt += "<td>" + pulseEntry.lastMsg.length + "</td>"; //pulse size
                         txt += '<td class="'+pulseEntry.geo+'_owls"'+'>' + pulseEntry.owls.substring(0,20) + "</td>"
