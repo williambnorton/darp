8f16a7e1c8e7741c77e967b3116c525af7c9a5ee
William B. Norton <wbn@drpeering.net>
2020-05-16 09:40:50 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- scripts/configWG.bash
+++ scripts/configWG.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #       configWG.bash - Configure Wireguard public and private for DARP 
-#			~/wireguard outside the Docker is our /etc/wireguard
+#			~/wireguard outside the Docker is our $DARPDIR/wireguard
 #			(outside Docker watcher script will re-run wg-quick UP)
 #
 #GENESIS=71.202.2.184:65013
@@ -13,8 +13,8 @@
 #fi
 echo `date` $0 creating wireguard configuration from $GENESIS
 DARPDIR=~/darp
-mkdir -p $DARPDIR/etc/wireguard
-cd $DARPDIR/etc/wireguard
+mkdir -p $DARPDIR/wireguard
+cd $DARPDIR/wireguard
 umask 077
 echo `date` "This will create publickey and privatekey We do not want them to include space characters, braces or brackets or commas"
 DONE=0
@@ -45,19 +45,18 @@ done
 echo PRIVATEKEY=$PRIVATEKEY PUBLICKEY=$PUBLICKEY
 #
 #	The wgbase model will be used to re-build the conf file
-# Note: for security reasons we don't NEED to store privatekey on disk
-#       in addition to our wireguard conf. No need to privatekey or publickey files
-touch $DARPDIR/etc/wireguard/wgbase.conf $DARPDIR/etc/wireguard/wg0.conf
-chmod 600 $DARPDIR/etc/wireguard/wg0.conf $DARPDIR/etc/wireguard/wgbase.conf
+# Note: for security reasons we don't NEED to store privatekey in VM
+touch $DARPDIR/wireguard/wgbase.conf $DARPDIR/wireguard/wg0.conf
+chmod 600 $DARPDIR/wireguard/wg0.conf $DARPDIR/wireguard/wgbase.conf
 
-echo ''>$DARPDIR/etc/wireguard/wgbase.conf
-echo '# Created by '$0 `date` >> $DARPDIR/etc/wireguard/wgbase.conf
-echo '[Interface]'>>$DARPDIR/etc/wireguard/wgbase.conf
-echo "PrivateKey = $PRIVATEKEY" >>$DARPDIR/etc/wireguard/wgbase.conf
-echo "# my PublicKey to share is $PUBLICKEY" >>$DARPDIR/etc/wireguard/wgbase.conf
-echo "#" >>$DARPDIR/etc/wireguard/wgbase.conf
+echo ''>$DARPDIR/wireguard/wgbase.conf
+echo '# Created by '$0 `date` >> $DARPDIR/wireguard/wgbase.conf
+echo '[Interface]'>>$DARPDIR/wireguard/wgbase.conf
+echo "PrivateKey = $PRIVATEKEY" >>$DARPDIR/wireguard/wgbase.conf
+echo "# my PublicKey to share is $PUBLICKEY" >>$DARPDIR/wireguard/wgbase.conf
+echo "#" >>$DARPDIR/wireguard/wgbase.conf
 
-cp $DARPDIR/etc/wireguard/wgbase.conf $DARPDIR/etc/wireguard/wg0.conf
+cp $DARPDIR/wireguard/wgbase.conf $DARPDIR/wireguard/wg0.conf
 
-echo `date` $0 wgbase.conf below - the rest will be added by noiaSR c ode
-cat $DARPDIR/etc/wireguard/wgbase.conf
+echo `date` $0 wgbase.conf below - the rest will be added by running code
+cat $DARPDIR/wireguard/wgbase.conf
