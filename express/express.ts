9b03c722da602e7a035a9d17c3ad5b6f84f6947e
William B. Norton <wbn@drpeering.net>
2020-05-22 12:59:27 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.ts
+++ express/express.ts
@@ -241,10 +241,13 @@ function handleShowState(req, res) {
        
        txt += '          for (let [field, fieldValue] of Object.entries(pulse)) {'
        // txt += '           console.log("     FOR EACH FIELD       ^field="+field+" fieldValue="+fieldValue);'
-       txt += '              console.log("Setting "+pulse.geo+"_"+field+"="+fieldValue);'
+       //txt += '              console.log("Setting "+pulse.geo+"_"+field+"="+fieldValue);'
 
         txt += '             $("."+pulse.geo+"_"+field).html(fieldValue+"");'
         txt += '         }'
+
+       txt += '          console.log("config="+dump(config));'
+
 
         txt += '          if (pulse.pulseTimestamp!="0")'
         txt += '              $("."+pulse.geo+"_pulseTimestamp").html(""+Math.round((now-pulse.pulseTimestamp)/1000)+" secs ago");'
