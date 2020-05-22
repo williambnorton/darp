45f75a5253b55bf7b3c4e5deed660cb104ae7cc9
William B. Norton <wbn@drpeering.net>
2020-05-22 07:54:15 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.ts
+++ express/express.ts
@@ -259,7 +259,7 @@ function handleShowState(req, res) {
 
 
         txt +='           var classification=Math.round(100*(Math.abs(pulse.median-pulse.owl)/pulse.median));'
-        //txt +='           console.log("pulse.owl="+pulse.owl+" pulse.median="+pulse.median+" classification="+classification);'
+        txt +='           console.log("pulse.owl="+pulse.owl+" pulse.median="+pulse.median+" classification="+classification);'
         //txt += '          if (classification>50) $("."+pulse.srcMint+"-"+"'+me.mint+'").css("background-color","red");'
        
 
