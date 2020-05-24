37ab8f61ef9fbfd73b9c5355b567ff2d66be7233
William B. Norton <wbn@drpeering.net>
2020-05-24 07:30:45 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.js
+++ express/express.js
@@ -264,9 +264,9 @@ function handleShowState(req, res) {
         //
         txt += '               if (!isNaN(owl)) owl="";';
         txt += '               ';
-        txt += '               var linkToMe=\'<a target="_blank" href="http://' + me.ipaddr + ':' + me.port + '/graph?srcMint=\';';
-        txt += '               linkToMe += srcMint + "&dstMint=" + dstMint+ "&group=" + "' + me.group + '"+ \'">\' + owl + "ms </a>";';
-        txt += '               $("."+srcMint+"-"+dstMint).html(linkToMe);'; //set owl value
+        txt += '               var owlHTML=\'<a target="_blank" href="http://' + me.ipaddr + ':' + me.port + '"/graph?srcMint="+ srcMint + "&dstMint=" + dstMint+ "&group=" + "' + me.group + '"+ \'">\' + owl + "ms </a>";';
+        txt += '               console.log("owlHTML="+owlHTML);';
+        txt += '               $("."+srcMint+"-"+dstMint).html(owlHTML);'; //set owl value
         txt += '                  if (!isNaN(owl) || !isNaN(myMedian)) $("."+srcMint+"-"+dstMint).css("background-color","white");'; //no owl or median - blank white
         txt += '                  else if (srcMint!=dstMint) ';
         txt += '                       if ((typeof Ideviation == "number") && (Ideviation>30))      $("."+srcMint+"-"+dstMint).css("background-color","lightred");';
