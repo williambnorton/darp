d253b9a8a1e8ad469cc6ff995aa97d004bdab5d5
William B. Norton <wbn@drpeering.net>
2020-05-25 07:40:26 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.js
+++ express/express.js
@@ -293,7 +293,7 @@ function handleShowState(req, res) {
         txt += '               const mean = owls.reduce((a,b) => a+b)/n;';
         txt += '               const s = Math.sqrt(owls.map(x => Math.pow(x-mean,2)).reduce((a,b) => a+b)/n);';
         txt += '               var percentOfMedian=Math.round((s/myMedian)*100);';
-        txt += '               console.log("stddev="+s+" percentOfMedian="+percentOfMedian);';
+        txt += '               console.log(srcMint+"-"+dstMint+" owl="+owl+" myMedian="+myMedian+" Idevitation=:"+Ideviation+" stddev="+s+" percentOfMedian="+percentOfMedian);';
         txt += '               if (percentOfMedian>10) $("."+srcMint+"-"+dstMint).css("border-color","yellow");';
         txt += '               if (percentOfMedian>20) $("."+srcMint+"-"+dstMint).css("border-color","orange");';
         txt += '               if (percentOfMedian>30) $("."+srcMint+"-"+dstMint).css("border-color","red");';
