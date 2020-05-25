a041ff3d5bc636e9ca2196d7ced1d2e9a3bceac5
William B. Norton <wbn@drpeering.net>
2020-05-25 09:05:59 -0700

    Version 1.0 - basic pulses and instrumentation of OWL Matrix in Redis + stabliizing base platform for launch

--- express/express.ts
+++ express/express.ts
@@ -353,7 +353,6 @@ function handleShowState(req, res) {
         txt += "    setTimeout(fetchState,1000);"
         txt += "}"
         txt += "function getOWL(config,src,dst) { "
-        txt += "        for (var pulse in configs[config].pulses) {"
         txt += "            var pulseEntry=configs[config].pulses[pulse];"  //convenience
         txt += "            if (pulseEntry.srcMint==dst) { "   //find receiver and check owls
         txt += '               var owls=pulseEntry.owls.split(",");'
@@ -371,7 +370,6 @@ function handleShowState(req, res) {
         txt += '                  }'
         txt += '               }'
         txt += '            }'
-        txt += "         }"
 
         txt += "    return 0;"
         txt += "}"
