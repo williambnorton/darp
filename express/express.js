961800aecfaa292204b704848b987fb33f7c848b
William B. Norton <wbn@drpeering.net>
2020-05-25 07:32:12 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.js
+++ express/express.js
@@ -289,14 +289,17 @@ function handleShowState(req, res) {
         txt += '               else if ((typeof Ideviation == "number") && (Ideviation>' + ORANGE_TRIGGER + ')) $("."+srcMint+"-"+dstMint).css("background-color","orange");';
         txt += '                  else if ((typeof Ideviation == "number") && (Ideviation>' + YELLOW_TRIGGER + ')) $("."+srcMint+"-"+dstMint).css("background-color","yellow");';
         txt += '                    else $("."+srcMint+"-"+dstMint).css("background-color","lightGreen");';
-        txt += "               const n = owls.length;";
-        txt += '               const mean = owls.reduce((a,b) => a+b)/n;';
-        txt += '               const s = Math.sqrt(owls.map(x => Math.pow(x-mean,2)).reduce((a,b) => a+b)/n);';
-        txt += '               var percentOfMedian=Math.round((s/myMedian)*100)';
-        txt += '               console.log("stddev="+s+" percentOfMedian="+percentOfMedian);';
-        txt += '               if (percentOfMedian>10) $("."+srcMint+"-"+dstMint).css("background-color","pink");';
-        txt += '               if (percentOfMedian>20) $("."+srcMint+"-"+dstMint).css("background-color","purple");';
-        txt += '               if (percentOfMedian>30) $("."+srcMint+"-"+dstMint).css("background-color","grey");';
+        /*
+                txt += "               const n = owls.length;"
+                txt += '               const mean = owls.reduce((a,b) => a+b)/n;'
+                txt += '               const s = Math.sqrt(owls.map(x => Math.pow(x-mean,2)).reduce((a,b) => a+b)/n);'
+        
+                txt += '               var percentOfMedian=Math.round((s/myMedian)*100);'
+                txt += '               console.log("stddev="+s+" percentOfMedian="+percentOfMedian);'
+                txt += '               if (percentOfMedian>10) $("."+srcMint+"-"+dstMint).css("background-color","pink");'
+                txt += '               if (percentOfMedian>20) $("."+srcMint+"-"+dstMint).css("background-color","purple");'
+                txt += '               if (percentOfMedian>30) $("."+srcMint+"-"+dstMint).css("background-color","grey");'
+        */
         txt += '            }';
         txt += '         }';
         txt += '      }';
