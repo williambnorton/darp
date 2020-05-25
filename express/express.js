35f5e3425ca5154c3a81efa522ec3ceca47dc08a
William B. Norton <wbn@drpeering.net>
2020-05-25 07:34:23 -0700

    Getting Basic Instant Overlay + stabliizing base platform for launch

--- express/express.js
+++ express/express.js
@@ -294,8 +294,8 @@ function handleShowState(req, res) {
         txt += '               const s = Math.sqrt(owls.map(x => Math.pow(x-mean,2)).reduce((a,b) => a+b)/n);';
         txt += '               var percentOfMedian=Math.round((s/myMedian)*100);';
         txt += '               console.log("stddev="+s+" percentOfMedian="+percentOfMedian);';
+        txt += '               if (percentOfMedian>10) $("."+srcMint+"-"+dstMint).css("background-color","pink");';
         /*
-                txt += '               if (percentOfMedian>10) $("."+srcMint+"-"+dstMint).css("background-color","pink");'
                 txt += '               if (percentOfMedian>20) $("."+srcMint+"-"+dstMint).css("background-color","purple");'
                 txt += '               if (percentOfMedian>30) $("."+srcMint+"-"+dstMint).css("background-color","grey");'
         */
--- express/express.ts
+++ express/express.ts
@@ -330,8 +330,9 @@ function handleShowState(req, res) {
 
         txt += '               var percentOfMedian=Math.round((s/myMedian)*100);'
         txt += '               console.log("stddev="+s+" percentOfMedian="+percentOfMedian);'
-/*
+
         txt += '               if (percentOfMedian>10) $("."+srcMint+"-"+dstMint).css("background-color","pink");'
+/*
         txt += '               if (percentOfMedian>20) $("."+srcMint+"-"+dstMint).css("background-color","purple");'
         txt += '               if (percentOfMedian>30) $("."+srcMint+"-"+dstMint).css("background-color","grey");'
 */
