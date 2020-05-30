623388a0d925beda9a47c8ee9102e958f699b3ea
William B. Norton <wbn@drpeering.net>
2020-05-30 06:37:53 -0700

    Version 1.0 - basic pulses and instrumentation of OWL Matrix in Redis + stabliizing base platform for launch

--- express/express.js
+++ express/express.js
@@ -194,7 +194,7 @@ function getMatrixTable(config, darp, callback) {
 //
 function handleShowState(req, res) {
     var dateTime = new Date();
-    var txt = '<!DOCTYPE html><meta http-equiv="refresh" content="' + 60 + '">';
+    var txt = '<!DOCTYPE html><meta http-equiv="refresh" content="' + 120 + '">';
     expressRedisClient.hgetall("mint:0", function (err, me) {
         if (me == null)
             return console.log("handleShowState(): WEIRD: NULL mint:0");
