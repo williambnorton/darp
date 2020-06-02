d03be307293a19b666751024afb760f1a9fea3d4
William B. Norton <wbn@drpeering.net>
2020-06-02 08:19:50 -0700

    Version 1.0 - basic pulses and instrumentation of OWL Matrix in Redis + stabliizing base platform for launch

--- express/express.js
+++ express/express.js
@@ -341,20 +341,18 @@ function handleShowState(req, res) {
         txt += '      configs.unshift(config);'; //push onto front of stack
         txt += '      if ( configs.length > ' + MAX_CONFIG_FRAMES + ' ) configs.pop();'; //pop off end of stack (60 seconds worth kept)
         txt += '      if (FREEZEBTN=="FREEZE") renderPage(config);';
+        txt += '      if ( nodeCountLastTime > 1 ) {';
+        txt += '         var nodeCountNow=config.gSRlist.length;';
         /*
-        
-               txt += '      if ( nodeCountLastTime > 1 ) {'
-               txt += '         var nodeCountNow=config.gSRlist.length;'
                txt += '         console.log("nodeCountNow="+nodeCountNow+" nodeCountLastTime="+nodeCountLastTime );'
                txt += '         if (nodeCountLastTime!=nodeCountNow) {'
                txt += '             console.log("HERE I WOULD RELOAD(): nodeCountNow="+nodeCountNow"+" nodeCountLastTime="+nodeCountLastTime );'
         
                txt += '         }'
                txt += '      '
-               txt += '         nodeCountLastTime=nodeCountNow;'
-               txt += '      }'
-        
         */
+        txt += '         nodeCountLastTime=nodeCountNow;';
+        txt += '      }';
         txt += '   });';
         //TODO: This should be a full mesh of unique active mints from gSRlist
         //txt += "function mystddev(array) {"
