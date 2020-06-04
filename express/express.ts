25efcac737021e50d3196acccd09a41ae9f8af01
William B. Norton <wbn@drpeering.net>
2020-06-03 18:11:16 -0700

    Version 1.0 - basic pulses and instrumentation of OWL Matrix in Redis + stabliizing base platform for launch

--- express/express.ts
+++ express/express.ts
@@ -628,7 +628,7 @@ function handleShowState(req, res) {
 
                    txt += '<td class="'+pulseEntry.geo+'_version"'+'>' + pulseEntry.version + "</td>";
 
-                   var balance=(pulseEntry.inOctets+pulseEntry.outOctets)/1000000;
+                   var balance=(parseInt(pulseEntry.inOctets)+parseInt(pulseEntry.outOctets))/(1000000**2);
                    txt += '<td class="'+pulseEntry.geo+'_balance"' + '> $' + balance + "</td>";
 
                    //txt+="<td>"+pulseEntry.lastMsg+"</td>"
