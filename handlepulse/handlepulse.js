f3e8984aa7bb1a352cbcd7d7ed13b7a18828cb44
William B. Norton <wbn@drpeering.net>
2020-06-08 07:30:57 -0700

    Version 1.0 - basic pulses and instrumentation of OWL Matrix in Redis + stabliizing base platform for launch

--- handlepulse/handlepulse.js
+++ handlepulse/handlepulse.js
@@ -105,7 +105,7 @@ server.on('message', function (message, remote) {
     //if (SHOWPULSES == "1")
     console.log(lib_js_1.ts() + "HANDLEPULSE: received pulse " + message.length + " bytes from " + remote.address + ':' + remote.port + ' - ' + message /*+dump(remote)*/);
     console.log("pushing onto msgQ : " + message);
-    redisClient.lpush(['pulseMsgQ', { incomingTimestamp: "" + lib_js_1.now(), message: message }]);
+    redisClient.rpush(['pulseMsgQ', JSON.stringify({ incomingTimestamp: "" + lib_js_1.now(), message: message })]);
 });
 /****
   var msg = message.toString();
