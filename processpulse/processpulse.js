5b322198907dd88f040499d6dbdd19b184f701d8
William B. Norton <wbn@drpeering.net>
2020-06-09 06:12:17 -0700

    Version 0.1 - Moving to platform layer model- messageBus on Redis,stats calc out of browser + stabliizing base platform for launch

--- processpulse/processpulse.js
+++ processpulse/processpulse.js
@@ -90,7 +90,7 @@ function waitForPush() {
             var OWL = incomingTimestamp - pulseTimestamp;
             //            console.log("measured OWL="+OWL+" for message="+message);
             var owlsStart = nth_occurrence(message, ',', 8); //owls start after the 7th comma
-            var pulseOwls = message.substring(owlsStart + 1, message.length - 1);
+            var pulseOwls = message.substring(owlsStart + 1, message.length);
             console.log("1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
             console.log("message=" + message + " owlstart=" + owlsStart, " pulseOwls=" + pulseOwls);
             var pulse = {
