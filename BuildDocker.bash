2c0a2268bef8768e881cb11d2b2904916656064a
William B. Norton <wbn@drpeering.net>
2020-06-18 14:42:07 -0700

    Version 0.1 - Moving to platform layer model- messageBus on Redis,stats calc out of browser + stabliizing base platform for launch

--- BuildDocker.bash
+++ BuildDocker.bash
@@ -2,12 +2,8 @@
 #   buildDocker.bash - build a new docker - takes 5 minutes
 #
 echo `date` Building darp codebase 
-#cd darp
 ./builddarp.bash 
 echo `date` Building the docker container
-cd ..
 docker build --no-cache -t williambnorton/darp . && docker push williambnorton/darp
-
-cd darp
-echo `date` Built docker `ls Build*`
+echo `date` New docker `ls Build*`
 echo `date` Finished $0 
