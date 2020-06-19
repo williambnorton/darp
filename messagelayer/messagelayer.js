2e6476d9c42e12b63964382e09677581e14b6de9
William B. Norton <wbn@drpeering.net>
2020-06-19 16:28:02 -0700

    Version 0.1 - Moving to platform layer model- messageBus on Redis,stats calc out of browser + stabliizing base platform for launch

--- messagelayer/messagelayer.js
+++ messagelayer/messagelayer.js
@@ -23,15 +23,26 @@ server.bind(PORT);
 // Prints: server listening 0.0.0.0:41234
 //--------------------------------------------------------------------
 //    SENDER CODE
-var pulseMessage = "" + now() + ",0,Build.200619.1110,DEVOPS,DEVOPS.1,194,1592591506442,1592590923743,1,2,1,";
 //pulseMsg sample: 0,Build.200619.1110,DEVOPS,DEVOPS.1,194,1592591506442,1592590923743,1,2,1, from 71.202.2.184:64339
-function sendMessage(message, ipaddr, port) {
-    console.log(ts() + "sending " + message + " to " + ipaddr + ":" + port);
-    var client = dgram.createSocket('udp4');
-    client.send(message, port, ipaddr, function (err) {
-        client.close();
+var client = dgram.createSocket('udp4');
+//
+//   send(): 
+//
+function send(rawmsg, nodelist) {
+    nodelist.forEach(function (node) {
+        var ipaddr = node.split(":")[0];
+        var port = node.split(":")[1] || "65013";
+        var message = Buffer.from(now() + "," + rawmsg);
+        console.log(ts() + "sending " + message + " to " + ipaddr + ":" + port);
+        client.send(message, 0, message.length, port, ipaddr, function (err) {
+            if (err) {
+                console.log("sendMessage(): ERROR");
+                client.close();
+            }
+        });
     });
 }
+var pulseMessage = "incomingTimestamp=" + now() + ",0,Build.200619.1110,DEVOPS,DEVOPS.1,194,1592591506442,1592590923743,1,2,1,";
 function ts() {
     return new Date().toLocaleTimeString() + " ";
 }
@@ -44,12 +55,12 @@ function now() {
 process.argv.shift(); //ignore rid of node
 process.argv.shift(); //ignore rid of path to mthis code
 function test_app_pulser() {
-    process.argv.forEach(function (val) {
-        var ipaddr = val.split(":")[0];
-        var port = val.split(":")[1] || "65013";
-        var message = Buffer.from(pulseMessage);
-        sendMessage(message, ipaddr, port);
-    });
+    //process.argv.forEach(function (val) {
+    //const ipaddr=val.split(":")[0];
+    //const port=val.split(":")[1]||"65013";
+    //const message = Buffer.from(pulseMessage);
+    send(pulseMessage, process.argv);
+    //});
     setTimeout(test_app_pulser, 1000);
 }
 test_app_pulser(); //bench test - uncomment to run a test
--- messagelayer/messagelayer.ts
+++ messagelayer/messagelayer.ts
@@ -29,19 +29,25 @@ server.bind(PORT);
 //--------------------------------------------------------------------
 //    SENDER CODE
 
-
-var pulseMessage=""+now()+",0,Build.200619.1110,DEVOPS,DEVOPS.1,194,1592591506442,1592590923743,1,2,1,";
-
-
 //pulseMsg sample: 0,Build.200619.1110,DEVOPS,DEVOPS.1,194,1592591506442,1592590923743,1,2,1, from 71.202.2.184:64339
-function sendMessage(message,ipaddr,port) {
-  
-  console.log(ts()+"sending "+message+" to "+ipaddr+":"+port);
-  const client = dgram.createSocket('udp4');
-  client.send(message, port, ipaddr, (err) => {
-    client.close();
+const client = dgram.createSocket('udp4');
+
+//
+//   send(): 
+//
+function send(rawmsg:string,nodelist:string[]) {
+  nodelist.forEach(function (node:string) {
+    const ipaddr=node.split(":")[0];
+    const port=node.split(":")[1]||"65013";
+    const message = Buffer.from(now()+","+rawmsg);
+    console.log(ts()+"sending "+message+" to "+ipaddr+":"+port);
+    client.send(message, 0, message.length, port, ipaddr, (err:string) => {
+      if (err) {console.log(`sendMessage(): ERROR`);client.close();}
+    });
   });
 }
+
+var pulseMessage="incomingTimestamp="+now()+",0,Build.200619.1110,DEVOPS,DEVOPS.1,194,1592591506442,1592590923743,1,2,1,";
 
 function ts() {
   return new Date().toLocaleTimeString() + " ";
@@ -57,12 +63,12 @@ process.argv.shift();  //ignore rid of node
 process.argv.shift();  //ignore rid of path to mthis code
 
 function test_app_pulser() {
-  process.argv.forEach(function (val) {
-    const ipaddr=val.split(":")[0];
-    const port=val.split(":")[1]||"65013";
-    const message = Buffer.from(pulseMessage);
-    sendMessage(message,ipaddr,port);
-  });
+  //process.argv.forEach(function (val) {
+    //const ipaddr=val.split(":")[0];
+    //const port=val.split(":")[1]||"65013";
+    //const message = Buffer.from(pulseMessage);
+    send(pulseMessage,process.argv);
+  //});
   setTimeout(test_app_pulser,1000);
 }
 test_app_pulser();  //bench test - uncomment to run a test
