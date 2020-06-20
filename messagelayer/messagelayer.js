b6f28c0a42fa7366f15cb956114fdab111e74265
William B. Norton <wbn@drpeering.net>
2020-06-20 04:29:34 -0700

    Version 0.1 - Moving to platform layer model- messageBus on Redis,stats calc out of browser + stabliizing base platform for launch

--- messagelayer/messagelayer.js
+++ messagelayer/messagelayer.js
@@ -1,47 +1,61 @@
+"use strict";
+exports.__esModule = true;
 //
 //  -create the UDP message bus for communication with all nodes
 // all others only have to deal with message, we timestamp and queue it here
-var PORT = "65013";
+var PORT = process.env.PORT || "65013";
+var TEST = true;
 //
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
-//misc. routines
-function ts() {
-    return new Date().toLocaleTimeString() + " ";
-}
-function now() {
-    var d = new Date();
-    return d.getTime();
-}
+exports.stats = {
+    port: 0,
+    inMsgs: 0,
+    outMsgs: 0,
+    lastInTimestamp: 0,
+    lastOutTimestamp: 0,
+    lastInMsg: "",
+    lastOutMsg: ""
+};
 //  RECEIVER CODE
 server.on('error', function (err) {
     console.log("server error:\n" + err.stack);
     server.close();
 });
-server.on('message', function (msg, rinfo) {
-    var incomingTimestamp = now();
-    console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
-    var incomingMessage = incomingTimestamp + "," + msg; //prepend our timeStamp
-});
 server.on('listening', function () {
     var address = server.address();
     console.log("server listening " + address.address + ":" + address.port);
 });
-server.bind(PORT);
-// Prints: server listening 0.0.0.0:41234
+function recvMsg(port, callback) {
+    server.bind(PORT);
+    // Prints: server listening 0.0.0.0:41234
+    server.on('message', function (msg, rinfo) {
+        var incomingTimestamp = exports.stats.lastInTimestamp = now();
+        exports.stats.inMsgs++;
+        console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+        var incomingMessage = incomingTimestamp + "," + msg; //prepend our timeStamp
+        exports.stats.lastInMsg = incomingMessage;
+        callback(incomingMessage);
+    });
+}
+exports.recvMsg = recvMsg;
 //--------------------------------------------------------------------
 //    SENDER CODE
 //pulseMsg sample: 0,Build.200619.1110,DEVOPS,DEVOPS.1,194,1592591506442,1592590923743,1,2,1, from 71.202.2.184:64339
 var client = dgram.createSocket('udp4');
 //
-//   send(): 
+//   sendMsg(): Send same message to all nodes in nodelist 
 //
-function send(rawmsg, nodelist) {
+function sendMsg(outgoingMessage, nodelist) {
     nodelist.forEach(function (node) {
         var ipaddr = node.split(":")[0];
         var port = node.split(":")[1] || "65013";
-        var message = Buffer.from(now() + "," + rawmsg);
-        console.log(ts() + "sending " + message + " to " + ipaddr + ":" + port);
+        var timestampedMsg = "" + now() + "," + outgoingMessage;
+        var message = Buffer.from(timestampedMsg);
+        exports.stats.outMsgs++;
+        exports.stats.lastOutTimestamp = now();
+        exports.stats.lastOutMsg = timestampedMsg;
+        console.log(ts() + "messagelayer.sendMsg() sending " + timestampedMsg + " to " + ipaddr + ":" + port);
         client.send(message, 0, message.length, port, ipaddr, function (err) {
             if (err) {
                 console.log("sendMessage(): ERROR");
@@ -50,6 +64,7 @@ function send(rawmsg, nodelist) {
         });
     });
 }
+exports.sendMsg = sendMsg;
 var pulseMessage = "incomingTimestamp=" + now() + ",0,Build.200619.1110,DEVOPS,DEVOPS.1,194,1592591506442,1592590923743,1,2,1,";
 /************************/
 // launch with TEST=1 to get automatic pulser and catcher
@@ -60,8 +75,18 @@ function test_app_pulser() {
     //const ipaddr=val.split(":")[0];
     //const port=val.split(":")[1]||"65013";
     //const message = Buffer.from(pulseMessage);
-    send(pulseMessage, process.argv);
+    sendMsg(pulseMessage, process.argv);
     //});
-    setTimeout(test_app_pulser, 1000);
+    setTimeout(test_app_pulser, 1000); //do it again in a few seconds
 }
-test_app_pulser(); //bench test - uncomment to run a test
+if (TEST)
+    test_app_pulser(); //bench test - uncomment to run a test
+//==============
+//misc. routines
+function ts() {
+    return new Date().toLocaleTimeString() + " ";
+}
+function now() {
+    var d = new Date();
+    return d.getTime();
+}
--- messagelayer/messagelayer.ts
+++ messagelayer/messagelayer.ts
@@ -1,42 +1,49 @@
 //
 //  -create the UDP message bus for communication with all nodes
 // all others only have to deal with message, we timestamp and queue it here
-var PORT="65013"
-
+const PORT=process.env.PORT||"65013"
+const TEST=true;
 //
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 
-//misc. routines
-function ts() {
-  return new Date().toLocaleTimeString() + " ";
-}
-function now() {
-  var d = new Date();
-  return d.getTime();
-}
 
 
 
+export var stats={
+  port: 0,
+  inMsgs : 0,
+  outMsgs : 0,
+  lastInTimestamp: 0,
+  lastOutTimestamp :0,
+  lastInMsg :"",
+  lastOutMsg :""
+};
 //  RECEIVER CODE
 server.on('error', (err) => {
   console.log(`server error:\n${err.stack}`);
   server.close();
 });
 
-server.on('message', (msg, rinfo) => {
-  var incomingTimestamp=now();
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-  var incomingMessage=`${incomingTimestamp},${msg}` //prepend our timeStamp
-});
+
 
 server.on('listening', () => {
   const address = server.address();
   console.log(`server listening ${address.address}:${address.port}`);
 });
 
-server.bind(PORT);
-// Prints: server listening 0.0.0.0:41234
+export function recvMsg(port:string,callback:any) {
+  server.bind(PORT);
+  // Prints: server listening 0.0.0.0:41234
+  server.on('message', (msg, rinfo) => {
+    var incomingTimestamp=stats.lastInTimestamp=now();
+    stats.inMsgs++;
+    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    var incomingMessage=`${incomingTimestamp},${msg}` //prepend our timeStamp
+    stats.lastInMsg=incomingMessage;
+    callback(incomingMessage);
+  });
+}
 //--------------------------------------------------------------------
 //    SENDER CODE
 
@@ -44,14 +51,19 @@ server.bind(PORT);
 const client = dgram.createSocket('udp4');
 
 //
-//   send(): 
+//   sendMsg(): Send same message to all nodes in nodelist 
 //
-function send(rawmsg:string,nodelist:string[]) {
+export function sendMsg(outgoingMessage:string,nodelist:string[]) {
+
   nodelist.forEach(function (node:string) {
     const ipaddr=node.split(":")[0];
     const port=node.split(":")[1]||"65013";
-    const message = Buffer.from(now()+","+rawmsg);
-    console.log(ts()+"sending "+message+" to "+ipaddr+":"+port);
+    const timestampedMsg=""+now()+","+outgoingMessage;
+    const message = Buffer.from(timestampedMsg);
+    stats.outMsgs++;
+    stats.lastOutTimestamp=now();
+    stats.lastOutMsg=timestampedMsg;
+    console.log(ts()+"messagelayer.sendMsg() sending "+timestampedMsg+" to "+ipaddr+":"+port);
     client.send(message, 0, message.length, port, ipaddr, (err:string) => {
       if (err) {console.log(`sendMessage(): ERROR`);client.close();}
     });
@@ -60,20 +72,30 @@ function send(rawmsg:string,nodelist:string[]) {
 
 var pulseMessage="incomingTimestamp="+now()+",0,Build.200619.1110,DEVOPS,DEVOPS.1,194,1592591506442,1592590923743,1,2,1,";
 
-
-
 /************************/
 // launch with TEST=1 to get automatic pulser and catcher
 process.argv.shift();  //ignore rid of node
 process.argv.shift();  //ignore rid of path to mthis code
 
-function test_app_pulser() {
+function test_app_pulser() {    //sample test app 
   //process.argv.forEach(function (val) {
     //const ipaddr=val.split(":")[0];
     //const port=val.split(":")[1]||"65013";
     //const message = Buffer.from(pulseMessage);
-    send(pulseMessage,process.argv);
+    sendMsg(pulseMessage, process.argv);
   //});
-  setTimeout(test_app_pulser,1000);
+  setTimeout(test_app_pulser,1000);  //do it again in a few seconds
 }
-test_app_pulser();  //bench test - uncomment to run a test
+if (TEST) test_app_pulser();  //bench test - uncomment to run a test
+
+
+
+//==============
+//misc. routines
+function ts() {
+  return new Date().toLocaleTimeString() + " ";
+}
+function now() {
+  var d = new Date();
+  return d.getTime();
+}
