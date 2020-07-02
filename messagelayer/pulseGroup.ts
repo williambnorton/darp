5bc3fbc03a0de97739422e31b8b55745843dfb97
William B. Norton <wbn@drpeering.net>
2020-07-02 06:58:12 -0700

    Version 2 - simple javascript pulseGroup object model +

--- messagelayer/pulseGroup.ts
+++ messagelayer/pulseGroup.ts
@@ -207,7 +207,7 @@ txt += '      for (let [key, value] of Object.entries(pulseGroup.pulses)) {'
 
 //        txt +='           $("."+pulse.geo+"_owls").html(\'<span style="text-align:left>"\'+pulse.owls+"</span>");'  //TODO : Align left for this text field
    txt += '       }'   
-   txt += '       totalEarn=parseFloat(totalEarn);'
+   txt += '       totalEarn=parseFloat(totalEarn).toFixed();'
    txt +='        $(".total_earn").html(totalEarn);'  //TODO : Align left for this text field
 //   txt +='           $(".total_earn").html("$" + totalEarn.toFixed(6));'  //TODO : Align left for this text field
 
