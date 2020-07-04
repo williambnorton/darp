4b7907c3854bd04342eaad0441b4a5e8a96e66ce
William B. Norton <wbn@drpeering.net>
2020-07-04 07:15:25 -0700

    Version 2 - simple javascript pulseGroup object model +

--- messagelayer/pulseGroup.ts
+++ messagelayer/pulseGroup.ts
@@ -362,58 +362,60 @@ txt += '      for (let [key, value] of Object.entries(pulseGroup.pulses)) {'
         //console.log(ts()+"                            mintTable="+dump(mintTable));
         for (var a in pulseGroup.mintTable) {
             var mintEntry = pulseGroup.mintTable[a];
-            //console.log(ts()+"a="+a+" mintEntry"+dump(mintEntry));
-            txt += '<tr class="'+mintEntry.geo+'">';
-            //txt+="<td>"+mintEntry+"</td>"
-            txt += "<td>" + mintEntry.mint + "</td>";
-            txt += '<td class="' + mintEntry.state + '">' + '<a target="_blank" href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/" >' + mintEntry.geo + "</a></td>";
-            txt += "<td>" + mintEntry.port + "</td>";
-            txt += "<td>" + '<a target="_blank" href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/me" >' + mintEntry.ipaddr + "</a></td>";
-            txt += "<td>" + mintEntry.publickey.substring(0, 3) + "..." + mintEntry.publickey.substring(40, mintEntry.publickey.length) + "</td>";
-            txt += '<td class="' + mintEntry.geo + '_state' + ' ' + mintEntry.state + '">' + '<a target="_blank" href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/mintTable" >' + mintEntry.state + '</a>' + "</td>";
-            //                   txt += "<td>" + '<a href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/config" >' + mintEntry.state + '</a>' + "</td>"
-            //var deltaT = Math.round((now() - mintEntry.pulseTimestamp) / 1000) + " secs ago";
-            //if (mintEntry.pulseTimestamp == 0) deltaT = "0";
-            //txt += '<td class="'+mintEntry.geo+'_pulseTimestamp"'+'">' + deltaT + "</td>";
-            var deltaSeconds = Math.round((now() - mintEntry.lastPulseTimestamp) / 1000) + " secs ago";
-            if (mintEntry.lastPulseTimestamp == 0)
-                deltaSeconds = "0";
-            //txt += "<td>" + now()+" "+entry.pulseTimestamp+ "</td>";
-            txt += '<td class="' + mintEntry.geo + '_pulseTimestamp"' + '>' + deltaSeconds + "</td>";
-//            txt += '<td class="' + mintEntry.geo + '_owl fade-out"' + '>' + '<a target="_blank" href="http://' + me.ipaddr + ':' + me.port + '/graph?src=' + mintEntry.geo + '&dst=' + me.geo + "&group=" + pulseGroup.groupName + '" >' + mintEntry.lastOWL + "</a> ms</td>";
-            txt += '<td class="' + mintEntry.geo + '_owl "' + '>' + '<a target="_blank" href="http://' + me.ipaddr + ':' + me.port + '/graph?src=' + mintEntry.geo + '&dst=' + me.geo + "&group=" + pulseGroup.groupName + '" >' + mintEntry.lastOWL + "</a> ms</td>";
-            //txt+="<td>"+mintEntry.bootTimestamp+"</td>"
-            txt += "<td>" + '<a target="_blank" href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/version" >' + mintEntry.version + "</a></td>";
-            txt += "<td>" + mintEntry.wallet.substring(0, 3) + "..." + mintEntry.wallet.substring(40, mintEntry.wallet.length) + "</td>";
-            //txt+="<td>"+mintEntry.SHOWPULSES+"</td>"
-            //txt += "<td>" + mintEntry.owl + " ms</td>"
-            //txt+="<td>"+mintEntry.isGenesisNode+"</td>"
-            //            txt+="<td>"+mintEntry.rtt+"</td>"
-            var stopButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/stop";
-            var rebootButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/reboot";
-            var reloadButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/reload";
-            var SINGLESTEPButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/SINGLESTEP";
-            var pulseMsgButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/pulseMsg";
-            txt += "<td>" + '<FORM>';
-            txt += '<INPUT Type="BUTTON" Value="PULSE1" Onclick="window.location.href=\'' + pulseMsgButtonURL + "'" + '">';
-            txt += '<INPUT Type="BUTTON" Value="RELOAD" Onclick="window.location.href=\'' + reloadButtonURL + "'" + '">';
-            txt += '<INPUT Type="BUTTON" Value="SINGLESTEP" Onclick="window.location.href=\'' + SINGLESTEPButtonURL + "'" + '">';
-            txt += '<INPUT Type="BUTTON" Value="STOP" Onclick="window.location.href=\'' + stopButtonURL + "'" + '">';
-            txt += '<INPUT Type="BUTTON" Value="REBOOT" Onclick="window.location.href=\'' + rebootButtonURL + "'" + '">';
-            txt += '</FORM>' + "</td>";
-            //if (mintEntry.adminControl)
-            //    txt += "<td>" + mintEntry.adminControl + "</td>";
-            //else
-                txt += "<td>" + "</td>";
-            //var delta = Math.round((now() - mintEntry.bootTimestamp) / 1000) + " secs ago";
-            //if (pulseEntry.bootTimestamp == 0) delta = "0";
-            //txt += '<td class="'+pulseEntry.geo+'_bootTimestamp"'+'">' + delta + "</td>";
-            var deltaSeconds2 = Math.round((now() - mintEntry.bootTimestamp) / 1000) + " secs ago";
-            if (mintEntry.bootTimestamp == 0)
-                deltaSeconds2 = "0";
-            //txt += "<td>" + now()+" "+entry.pulseTimestamp+ "</td>";
-            txt += '<td class="' + mintEntry.geo + '_bootTimestamp"' + '>' + deltaSeconds2 + "</td>";
-            txt += "</tr>";
+            if (mintEntry!=null) {
+                //console.log(ts()+"a="+a+" mintEntry"+dump(mintEntry));
+                txt += '<tr class="'+mintEntry.geo+'">';
+                //txt+="<td>"+mintEntry+"</td>"
+                txt += "<td>" + mintEntry.mint + "</td>";
+                txt += '<td class="' + mintEntry.state + '">' + '<a target="_blank" href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/" >' + mintEntry.geo + "</a></td>";
+                txt += "<td>" + mintEntry.port + "</td>";
+                txt += "<td>" + '<a target="_blank" href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/me" >' + mintEntry.ipaddr + "</a></td>";
+                txt += "<td>" + mintEntry.publickey.substring(0, 3) + "..." + mintEntry.publickey.substring(40, mintEntry.publickey.length) + "</td>";
+                txt += '<td class="' + mintEntry.geo + '_state' + ' ' + mintEntry.state + '">' + '<a target="_blank" href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/mintTable" >' + mintEntry.state + '</a>' + "</td>";
+                //                   txt += "<td>" + '<a href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/config" >' + mintEntry.state + '</a>' + "</td>"
+                //var deltaT = Math.round((now() - mintEntry.pulseTimestamp) / 1000) + " secs ago";
+                //if (mintEntry.pulseTimestamp == 0) deltaT = "0";
+                //txt += '<td class="'+mintEntry.geo+'_pulseTimestamp"'+'">' + deltaT + "</td>";
+                var deltaSeconds = Math.round((now() - mintEntry.lastPulseTimestamp) / 1000) + " secs ago";
+                if (mintEntry.lastPulseTimestamp == 0)
+                    deltaSeconds = "0";
+                //txt += "<td>" + now()+" "+entry.pulseTimestamp+ "</td>";
+                txt += '<td class="' + mintEntry.geo + '_pulseTimestamp"' + '>' + deltaSeconds + "</td>";
+    //            txt += '<td class="' + mintEntry.geo + '_owl fade-out"' + '>' + '<a target="_blank" href="http://' + me.ipaddr + ':' + me.port + '/graph?src=' + mintEntry.geo + '&dst=' + me.geo + "&group=" + pulseGroup.groupName + '" >' + mintEntry.lastOWL + "</a> ms</td>";
+                txt += '<td class="' + mintEntry.geo + '_owl "' + '>' + '<a target="_blank" href="http://' + me.ipaddr + ':' + me.port + '/graph?src=' + mintEntry.geo + '&dst=' + me.geo + "&group=" + pulseGroup.groupName + '" >' + mintEntry.lastOWL + "</a> ms</td>";
+                //txt+="<td>"+mintEntry.bootTimestamp+"</td>"
+                txt += "<td>" + '<a target="_blank" href="http://' + mintEntry.ipaddr + ':' + mintEntry.port + '/version" >' + mintEntry.version + "</a></td>";
+                txt += "<td>" + mintEntry.wallet.substring(0, 3) + "..." + mintEntry.wallet.substring(40, mintEntry.wallet.length) + "</td>";
+                //txt+="<td>"+mintEntry.SHOWPULSES+"</td>"
+                //txt += "<td>" + mintEntry.owl + " ms</td>"
+                //txt+="<td>"+mintEntry.isGenesisNode+"</td>"
+                //            txt+="<td>"+mintEntry.rtt+"</td>"
+                var stopButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/stop";
+                var rebootButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/reboot";
+                var reloadButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/reload";
+                var SINGLESTEPButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/SINGLESTEP";
+                var pulseMsgButtonURL = "http://" + mintEntry.ipaddr + ":" + mintEntry.port + "/pulseMsg";
+                txt += "<td>" + '<FORM>';
+                txt += '<INPUT Type="BUTTON" Value="PULSE1" Onclick="window.location.href=\'' + pulseMsgButtonURL + "'" + '">';
+                txt += '<INPUT Type="BUTTON" Value="RELOAD" Onclick="window.location.href=\'' + reloadButtonURL + "'" + '">';
+                txt += '<INPUT Type="BUTTON" Value="SINGLESTEP" Onclick="window.location.href=\'' + SINGLESTEPButtonURL + "'" + '">';
+                txt += '<INPUT Type="BUTTON" Value="STOP" Onclick="window.location.href=\'' + stopButtonURL + "'" + '">';
+                txt += '<INPUT Type="BUTTON" Value="REBOOT" Onclick="window.location.href=\'' + rebootButtonURL + "'" + '">';
+                txt += '</FORM>' + "</td>";
+                //if (mintEntry.adminControl)
+                //    txt += "<td>" + mintEntry.adminControl + "</td>";
+                //else
+                    txt += "<td>" + "</td>";
+                //var delta = Math.round((now() - mintEntry.bootTimestamp) / 1000) + " secs ago";
+                //if (pulseEntry.bootTimestamp == 0) delta = "0";
+                //txt += '<td class="'+pulseEntry.geo+'_bootTimestamp"'+'">' + delta + "</td>";
+                var deltaSeconds2 = Math.round((now() - mintEntry.bootTimestamp) / 1000) + " secs ago";
+                if (mintEntry.bootTimestamp == 0)
+                    deltaSeconds2 = "0";
+                //txt += "<td>" + now()+" "+entry.pulseTimestamp+ "</td>";
+                txt += '<td class="' + mintEntry.geo + '_bootTimestamp"' + '>' + deltaSeconds2 + "</td>";
+                txt += "</tr>";
+            }
         }
         txt += "</table>";
 
