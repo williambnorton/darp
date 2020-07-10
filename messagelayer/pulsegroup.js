221895abe9fbb2c757ec4299edd0f9be8af6300d
William B. Norton <wbn@drpeering.net>
2020-07-10 06:44:46 -0700

    Version 2 - simple javascript pulseGroup object model +

--- messagelayer/pulsegroup.js
+++ messagelayer/pulsegroup.js
@@ -267,7 +267,7 @@ function instrumentation() {
         for (var src in pulseGroup.matrix) {
             var mintEntry = pulseGroup.mintTable[src]; //src mintEntry
             if (mintEntry.state == "UP")
-                txt += '<tr class="' + mintEntry.geo + ' UP"><td>' + mintEntry.geo + " " + mintEntry.mint + '</td>'; //heacer on left side
+                txt += '<tr class="' + mintEntry.geo + ' UP"><td><a target="_blank" href="http://' + mintEntry.ipaddr + ":" + mintEntry.port + '/">' + mintEntry.geo + " " + mintEntry.mint + '</a></td>'; //heacer on left side
             else
                 txt += '<tr class="' + mintEntry.geo + ' NR"><td>' + mintEntry.geo + " " + mintEntry.mint + '</td>'; //heacer on left side
             for (var dest in pulseGroup.matrix[src]) {
