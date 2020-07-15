1fe4ec0fa1b34f5cadb0a060714630ef6e503b17
William B. Norton <wbn@drpeering.net>
2020-07-15 11:04:54 -0700

    Simple javascript pulseGroup object model with graphs +

--- dist/pulsegroup.js
+++ dist/pulsegroup.js
@@ -213,8 +213,9 @@ function instrumentation() {
     txt += '           var regex = /@/;';
     txt += '           var flag=strOwl.match(regex);';
     txt += '           if (flag) {'; //We have an OWL measure that should be investigated
-    //txt += '               //console.log("found a flagged entry "+strOwl);';
+    txt += '             console.log("found a flagged entry "+strOwl);';
     txt += '               if (srcMintEntry && dstMintEntry) {';
+    txt += '                   console.log("HIGHLIGHTING ${srcMintEntry.geo}-${dstMintEntry.geo=");';
     txt += '                   $("."+srcMintEntry.mint+"-"+dstMintEntry.mint).addClass("BUSY");';
     //txt += '               } else {';
     //txt += '                  $("."+srcMintEntry.mint+"-"+dstMintEntry.mint).css("border-color", "gray");';
