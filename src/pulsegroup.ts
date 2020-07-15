e463c9c06aeb956a6c91ce21a063410e9e947443
William B. Norton <wbn@drpeering.net>
2020-07-14 17:18:09 -0700

    Simple javascript pulseGroup object model with graphs +

--- src/pulsegroup.ts
+++ src/pulsegroup.ts
@@ -1232,7 +1232,7 @@ getMyPulseGroupObject(GENESIS, PORT, function (newPulseGroup) {
                 //var deviation=Math.round(Math.abs(medianOfMedians-medianOfMeasures)*100/medianOfMedians);
                 var deviation=Math.round(Math.abs(medianOfMedians-nodeEntry.owl)*100/medianOfMedians);
                 console.log(`geo=${nodeEntry.geo} nodeEntry.owl=${nodeEntry.owl} medianOfMeasures=${medianOfMeasures} medianOfMedians=${medianOfMedians} deviation=${deviation}%`);
-                if ((nodeEntry.owl>3) && (deviation>30)) flag="*;" //deviation 30% from the median, flag
+                if ((nodeEntry.owl>3) && (deviation>30)) flag="*" //deviation 30% from the median, flag
             }
                 owls+=nodeEntry.mint+"="+nodeEntry.owl+flag+","
        });
