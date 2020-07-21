956a18eb7e983a5206ea4677d5cade88be9d0620
William B. Norton <wbn@drpeering.net>
2020-07-21 05:58:14 -0700

    DARP Protocol with matrix and basic data graphs +

--- src/pulsegroup.ts
+++ src/pulsegroup.ts
@@ -1688,6 +1688,7 @@ getMyPulseGroupObject(GENESIS, GENESISPORT, function (newPulseGroup) {
                         if (newPulseGroup.mintTable[mymint]!=null && newPulseGroup.mintTable[mymint].mint>1) {
                             //find each mint in the group owner announcement or delete/resync
                             var found=false;
+                            console.log(`owls=${owlsAry} looking for mint #${mymint}`);
                             for(var o in owlsAry) {
                                 var owlmint=owlsAry[o].split("=")[0];
                                 console.log(`owlmint =${owlmint} mymint=${mymint}`);
