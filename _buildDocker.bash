8050e2fbbdc426a99d5c7b1aabfb40cd566632bd
William B. Norton <wbn@drpeering.net>
2020-07-27 16:54:34 -0700

    DARP Protocol with matrix and basic data graphs +

--- _buildDocker.bash
+++ _buildDocker.bash
@@ -5,7 +5,7 @@ echo `date` Building darp codebase
 ./builddarp.bash 
 npm install && npm update
 echo `date` Building the docker container
-docker build --no-cache -t williambnorton/darp . && docker push williambnorton/darp
+docker build --no-cache -t williambnorton/darp . && docker push williambnorton/darp:dev
 echo `date` New docker `ls Build*`
 echo `date` Finished $0 Force genesis node to reload SW and the group will follow
 #./builddarp.bash #always force a updateSW
