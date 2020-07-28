eea9132d0b3c9861629545656397419ed30b3cca
William B. Norton <wbn@drpeering.net>
2020-07-28 16:59:31 -0700

    DARP Protocol with matrix and basic data graphs +

--- dist/pulsegroup.js
+++ dist/pulsegroup.js
@@ -538,7 +538,7 @@ var AugmentedPulseGroup = /** @class */ (function () {
                         var owlEntry = owlsAry[o];
                         var mint = parseInt(owlEntry.split("=")[0]);
                         var srcMintEntry = self.mintTable[mint];
-                        console.log("owlEntry=" + owlEntry + " mint=" + mint + " srcMintEntry=" + lib_1.dump(srcMintEntry)); //#1
+                        //console.log(`owlEntry=${owlEntry} mint=${mint} srcMintEntry=${srcMintEntry}`);    //#1
                         //console.log(`owlEntry=${owlEntry} mint=${mint} mintTable[mint]==${dump(self.mintTable[mint])}`);    //#2
                         if (srcMintEntry == null) {
                             console.log("We do not have this mint the group Owner announced mint: " + mint);
