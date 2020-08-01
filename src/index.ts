8fcbadaaed2c4fe66e9a0c080f9c4aa968c9479c
William B. Norton <wbn@drpeering.net>
2020-08-01 06:55:15 -0700

    DARP Protocol with matrix and basic data graphs +

--- src/index.ts
+++ src/index.ts
@@ -481,7 +481,7 @@ function instrumentation() {    //this should get its own file
 
    //txt += '                  console.log("*  PATH       "+srcEntry.geo+"-"+destEntry.geo+"="+direct+" through "+intermediaryEntry.geo+" intermediaryPathLatency="+intermediaryPathLatency+" delta="+delta);'
     
-    txt += '               if (srcToIntermediary!=-99999 && intermediaryToDest!= -99999 && delta<10) {';
+    txt += '               if (srcToIntermediary!=-99999 && intermediaryToDest!= -99999 && delta<-10) {';
     txt += '                  console.log("*  EXCEPTIONAL PATH       "+srcEntry.geo+"-"+destEntry.geo+"="+direct+" through "+intermediaryEntry.geo+" intermediaryPathLatency="+intermediaryPathLatency+" delta="+delta);'
     txt += '                  ';
     txt += '               }';
