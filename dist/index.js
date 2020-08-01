fd781283c692383b95e7112fca70bc38f16a4f14
William B. Norton <wbn@drpeering.net>
2020-08-01 06:51:43 -0700

    DARP Protocol with matrix and basic data graphs +

--- dist/index.js
+++ dist/index.js
@@ -444,7 +444,7 @@ function instrumentation() {
     txt += '               var intermediaryToDest=getOWLfrom(intermediaryEntry.mint,destEntry.owls);';
     txt += '               var intermediaryPathLatency=parseInt(srcToIntermediary)+parseInt(intermediaryToDest);';
     txt += '               var delta=intermediaryPathLatency-direct;';
-    txt += '                  console.log("*  PATH       "+srcEntry.geo+"-"+destEntry.geo+"="+direct+" through "+intermediaryEntry.geo+" intermediaryPathLatency="+intermediaryPathLatency+" delta="+delta);';
+    //txt += '                  console.log("*  PATH       "+srcEntry.geo+"-"+destEntry.geo+"="+direct+" through "+intermediaryEntry.geo+" intermediaryPathLatency="+intermediaryPathLatency+" delta="+delta);'
     txt += '               if (srcToIntermediary!=-99999 && intermediaryToDest!= -99999 && delta<2) {';
     txt += '                  console.log("*  EXCEPTIONAL PATH       "+srcEntry.geo+"-"+destEntry.geo+"="+direct+" through "+intermediaryEntry.geo+" intermediaryPathLatency="+intermediaryPathLatency+" delta="+delta);';
     txt += '                  ';
--- src/index.ts
+++ src/index.ts
@@ -479,7 +479,7 @@ function instrumentation() {    //this should get its own file
     txt += '               var intermediaryPathLatency=parseInt(srcToIntermediary)+parseInt(intermediaryToDest);'
     txt += '               var delta=intermediaryPathLatency-direct;'
 
-    txt += '                  console.log("*  PATH       "+srcEntry.geo+"-"+destEntry.geo+"="+direct+" through "+intermediaryEntry.geo+" intermediaryPathLatency="+intermediaryPathLatency+" delta="+delta);'
+   //txt += '                  console.log("*  PATH       "+srcEntry.geo+"-"+destEntry.geo+"="+direct+" through "+intermediaryEntry.geo+" intermediaryPathLatency="+intermediaryPathLatency+" delta="+delta);'
     
     txt += '               if (srcToIntermediary!=-99999 && intermediaryToDest!= -99999 && delta<2) {';
     txt += '                  console.log("*  EXCEPTIONAL PATH       "+srcEntry.geo+"-"+destEntry.geo+"="+direct+" through "+intermediaryEntry.geo+" intermediaryPathLatency="+intermediaryPathLatency+" delta="+delta);'
