3172d1c6ee09b18eb3654b5695270aa26d0dfae4
William B. Norton <wbn@drpeering.net>
2020-08-01 09:22:27 -0700

    DARP Protocol with matrix and basic data graphs +

--- src/index.ts
+++ src/index.ts
@@ -510,7 +510,7 @@ function instrumentation() {    //this should get its own file
     txt += '       $("#srclabel-dsrtlabel").remove();'
     txt += '    }'
     //    txt += '    $("#extraordinary").append("<tr><td>"+extraordinaryPath.aSide+"</td> <td>"+extraordinaryPath.zSide+"</td><td>"+extraordinaryPath.direct+"</td><td></td><td>"+extraordinaryPath.intermediary+"</td><td>"+extraordinaryPath.intermediaryPathLatency+"</td><td>"+extraordinaryPath.srcToIntermediary+"</td><td>"+extraordinaryPath.intermediaryToDest+"</td><td>"+extraordinaryPath.delta+"</td></tr>");   '
-    txt += '    $("#extraordinary").append("<tr id=\"extraordinaryPath.aSide-extraordinaryPath.zSide\"><td>"+extraordinaryPath.aSide+"</td> <td>"+extraordinaryPath.zSide+"</td><td>"+extraordinaryPath.direct+"</td><td></td><td>"+extraordinaryPath.intermediary+"</td><td>"+extraordinaryPath.intermediaryPathLatency+"</td><td>"+extraordinaryPath.delta+"</td><td></td><td>"+extraordinaryPath.srcToIntermediary+"</td><td>"+extraordinaryPath.intermediaryToDest+"</td></tr>");   '
+    txt += '    $("#extraordinary").append("<tr id=\"extraordinaryPath.aSide-extraordinaryPath.zSide\"><td>"+extraordinaryPath.ts+"</td><td>"+extraordinaryPath.aSide+"</td> <td>"+extraordinaryPath.zSide+"</td><td>"+extraordinaryPath.direct+"</td><td></td><td>"+extraordinaryPath.intermediary+"</td><td>"+extraordinaryPath.intermediaryPathLatency+"</td><td>"+extraordinaryPath.delta+"</td><td></td><td>"+extraordinaryPath.srcToIntermediary+"</td><td>"+extraordinaryPath.intermediaryToDest+"</td></tr>");   '
 
     txt += '}'
 
