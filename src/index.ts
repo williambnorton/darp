6dd66dae69633479f5c50c39b5135e9ab9991663
William B. Norton <wbn@drpeering.net>
2020-08-01 08:38:43 -0700

    DARP Protocol with matrix and basic data graphs +

--- src/index.ts
+++ src/index.ts
@@ -498,7 +498,7 @@ function instrumentation() {    //this should get its own file
     txt += 'for (var e in extraordinaryPaths) {'
     txt += '    var extraordinaryPath=extraordinaryPaths[e];'
     txt += '    console.log("extraordinaryPath: "+JSON.stringify(extraordinaryPath,null,2));'
-    txt += '    $("#extraordinary").append("<tr><td>"+extraordinaryPath.aSide+"</td> <td>"+extraordinaryPath.zSide+"</td><td>"+extraordinaryPath.direct+"</td><td></td><td>"+extraordinaryPath.intermediary+"</td><td>"+extraordinaryPath.srcTointermediary+"</td><td>"+extraordinaryPath.intermediaryToDest+"</td><td>"+extraordinaryPath.delta+"</td></tr>");   '
+    txt += '    $("#extraordinary").append("<tr><td>"+extraordinaryPath.aSide+"</td> <td>"+extraordinaryPath.zSide+"</td><td>"+extraordinaryPath.direct+"</td><td></td><td>"+extraordinaryPath.intermediary+"</td><td>"+extraordinaryPath.intermediaryPathLatency+"</td><td>"+extraordinaryPath.srcToIntermediary+"</td><td>"+extraordinaryPath.intermediaryToDest+"</td><td>"+extraordinaryPath.delta+"</td></tr>");   '
 
     txt += '}'
 
