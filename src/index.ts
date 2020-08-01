423261152b232a0c028698775d96d52589aed1ce
William B. Norton <wbn@drpeering.net>
2020-08-01 09:43:58 -0700

    DARP Protocol with matrix and basic data graphs +

--- src/index.ts
+++ src/index.ts
@@ -505,10 +505,10 @@ function instrumentation() {    //this should get its own file
     txt += '    var extraordinaryPath=extraordinaryPaths[e];'
     txt += '    console.log("extraordinaryPath: "+JSON.stringify(extraordinaryPath,null,2));'
     txt += '    var now=new Date();'
-//    txt += '    if (extraordinaryPath.ts-now.getTime() > 2000) {';
+    txt += '    if (extraordinaryPath.ts-now.getTime() > 2000) {';
 //    txt += '       console.log("deleting path "+JSON.stringify(extraordinaryPath,null,2));';
 //    txt += '       $("#srclabel-dsrtlabel").remove();'
-//    txt += '    }'
+    txt += '    }'
     //    txt += '    $("#extraordinary").append("<tr><td>"+extraordinaryPath.aSide+"</td> <td>"+extraordinaryPath.zSide+"</td><td>"+extraordinaryPath.direct+"</td><td></td><td>"+extraordinaryPath.intermediary+"</td><td>"+extraordinaryPath.intermediaryPathLatency+"</td><td>"+extraordinaryPath.srcToIntermediary+"</td><td>"+extraordinaryPath.intermediaryToDest+"</td><td>"+extraordinaryPath.delta+"</td></tr>");   '
     txt += '    $("#extraordinary").append("<tr><td>"+extraordinaryPath.ts+"</td><td>"+extraordinaryPath.aSide+"</td> <td>"+extraordinaryPath.zSide+"</td><td>"+extraordinaryPath.direct+"</td><td></td><td>"+extraordinaryPath.intermediary+"</td><td>"+extraordinaryPath.intermediaryPathLatency+"</td><td>"+extraordinaryPath.delta+"</td><td></td><td>"+extraordinaryPath.srcToIntermediary+"</td><td>"+extraordinaryPath.intermediaryToDest+"</td></tr>");   '
 
