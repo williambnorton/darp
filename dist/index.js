fb4bef92440400e3375d499c56d788429c57db2c
William B. Norton <wbn@drpeering.net>
2020-08-02 09:26:25 -0700

    DARP Protocol with matrix and basic data graphs +

--- dist/index.js
+++ dist/index.js
@@ -466,7 +466,7 @@ function instrumentation() {
     txt += '    var extraordinaryPath=extraordinaryPaths[e];';
     //txt += '    console.log("extraordinaryPath: "+JSON.stringify(extraordinaryPath,null,2));'
     txt += '    var now=new Date();';
-    txt += '    if (now.getTime()-extraordinaryPath.startTime>2000) delete extraordinaryPaths[e]; '; //delete extraordinary not extraordinary any more
+    txt += '    if (now.getTime()-extraordinaryPath.startTimestamp>2000) delete extraordinaryPaths[e]; '; //delete extraordinary not extraordinary any more
     txt += '    else { ';
     txt += '         var trLabel= "<tr id=" + extraordinaryPath.aSide+ "-" + extraordinaryPath.zSide + " >";';
     txt += '         console.log("trLabel="+trLabel+" extraordinaryPath="+JSON.stringify(extraordinaryPath, null, 2));';
