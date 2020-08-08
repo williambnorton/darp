f212b46fe8f484ae91eb89fadf8822610b7b8cdd
William B. Norton <wbn@drpeering.net>
2020-08-08 15:22:19 -0700

    DARP Protocol with matrix and basic data graphs +

--- dist/pulsegroup.js
+++ dist/pulsegroup.js
@@ -50,7 +50,7 @@ var wireguard_1 = require("./wireguard");
 // Define constants
 var PULSEFREQ = 1; // (in seconds) how often to send pulses
 var MEASURE_RTT = false; //ping across wireguard interface
-var FIND_EFFICIENCIES = true; //search for better paths through intermediaries
+var FIND_EFFICIENCIES = false; //search for better paths through intermediaries
 var WG_PULSEFREQ = 2; //send pings over wireguard mesh every other second
 var SECURE_PORT = 65020;
 var CHECK_SW_VERSION_CYCLE_TIME = 15; // CHECK SW updates every 15 seconds
--- src/pulsegroup.ts
+++ src/pulsegroup.ts
@@ -14,7 +14,7 @@ import { setWireguard, addPeerWGStanza, addMyWGStanza } from "./wireguard";
 // Define constants
 const PULSEFREQ=1;  // (in seconds) how often to send pulses
 const MEASURE_RTT=false;   //ping across wireguard interface
-const FIND_EFFICIENCIES=true; //search for better paths through intermediaries
+const FIND_EFFICIENCIES=false; //search for better paths through intermediaries
 const WG_PULSEFREQ=2; //send pings over wireguard mesh every other second
 const SECURE_PORT=65020;
 const CHECK_SW_VERSION_CYCLE_TIME = 15; // CHECK SW updates every 15 seconds
@@ -686,7 +686,7 @@ export class AugmentedPulseGroup {
     //
     //  @wbnwbnwbnwbn
     //
-    findEfficiencies = () => {
+    findEfficiencies = () => {      //run every second after timeout code
         if (!FIND_EFFICIENCIES) return;
         for (var srcP in this.pulses) {
             var srcEntry = this.pulses[srcP];
