4a7f2b496550e7f93ca869e97bea8bec25bddc43
William B. Norton <wbn@drpeering.net>
2020-08-10 13:12:35 -0700

    DARP Protocol with matrix and basic data graphs +

--- dist/pulsegroup.js
+++ dist/pulsegroup.js
@@ -759,7 +759,7 @@ var AugmentedPulseGroup = /** @class */ (function () {
         this.recvPulses = function () {
             var self = _this;
             pulselayer_1.recvPulses(_this.config.PORT, function (incomingPulse) {
-                console.log("recvPulse(): " + lib_1.dump(incomingPulse));
+                //console.log(`recvPulse(): ${dump(incomingPulse)}`);
                 self.incomingPulseQueue.push(incomingPulse); //tmp patch to test
             });
             console.log("************************** setting timeoput in 10ms to run worker thread incom,ingqueue length=" + self.incomingPulseQueue.length);
