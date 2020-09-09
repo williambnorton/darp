a17390da4f60b85cb368bcb5e6f38cccd3a24454
William B. Norton <wbn@drpeering.net>
2020-09-08 20:07:36 -0700

    DARP Protocol with matrix and basic data graphs +

--- src/index.ts
+++ src/index.ts
@@ -191,10 +191,11 @@ app.get(['/pulsegroups','/state','/me'], function(req, res) {
     //console.log(`sending contents of ${filename}`);
     try {
         var fileContents = fs.readFileSync(filename);
-        //var str=JSON.stringify(JSON.parse(fileContents),null,2);
-        //res.end(str); //CRASH - catch 
-        console.log(`filecontents=${fileContents}`);
-        res.end(fileContents); //CRASH - catch 
+        var str=JSON.stringify(JSON.parse(fileContents),null,2);
+        console.log(`filecontents=${str}`);
+
+        res.end(str); //CRASH - catch 
+        //res.end(fileContents); //CRASH - catch 
     } catch (err) {
         // Here you get the error when the file was not found,
         // but you also get any other error
