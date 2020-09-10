d5eae70c3b97c61ceb7d6beebc42a712ca29ff7f
William B. Norton <wbn@drpeering.net>
2020-09-10 16:25:29 -0700

    DARP Protocol with matrix and basic data graphs +

--- dist/sender.js
+++ dist/sender.js
@@ -2,9 +2,7 @@
 /** @module sender emits pulse message to a group of nodes */
 exports.__esModule = true;
 var dgram = require("dgram");
-var lib_1 = require("./lib");
 var logger_1 = require("./logger");
-var types_1 = require("./types");
 logger_1.logger.setLevel(logger_1.LogLevel.ERROR);
 var PULSE_INTERVAL = parseInt(process.argv[2]);
 var sender = dgram.createSocket("udp4");
@@ -12,20 +10,22 @@ var pulseGroupMap = new Map();
 process.on('message', function (senderMessage) {
     pulseGroupMap.set(senderMessage.type, senderMessage.payload);
 });
+/*
 // Send same message to all nodes in nodelist
-//setInterval(() => {
-var nodeList = pulseGroupMap.get(types_1.SenderPayloadType.NodeList);
-var outgoingMessage = pulseGroupMap.get(types_1.SenderPayloadType.OutgoingMessage);
-nodeList.forEach(function (node) {
-    var outgoingTimestamp = lib_1.now().toString();
-    var pulseMessage = outgoingTimestamp + "," + outgoingMessage;
-    var pulseBuffer = Buffer.from(pulseMessage, types_1.PulseMessageEncoding.latin1);
-    logger_1.logger.info("Sending " + pulseMessage + " to " + node.ipaddr + ":" + node.port);
-    console.log("Sending " + pulseMessage + " to " + node.ipaddr + ":" + node.port);
-    sender.send(pulseBuffer, 0, pulseBuffer.length, node.port, node.ipaddr, function (error) {
-        if (error) {
-            logger_1.logger.error("Sender error: " + error.message);
-        }
+setInterval(() => {
+    const nodeList: NodeAddress[] = pulseGroupMap.get(SenderPayloadType.NodeList)
+    const outgoingMessage: string = pulseGroupMap.get(SenderPayloadType.OutgoingMessage)
+    nodeList.forEach(function (node: NodeAddress) {
+        const outgoingTimestamp = now().toString();
+        const pulseMessage = outgoingTimestamp + "," + outgoingMessage;
+        const pulseBuffer = Buffer.from(pulseMessage, PulseMessageEncoding.latin1);
+        logger.info(`Sending ${pulseMessage} to ${node.ipaddr}:${node.port}`);
+        console.log(`Sending ${pulseMessage} to ${node.ipaddr}:${node.port}`);
+        sender.send(pulseBuffer, 0, pulseBuffer.length, node.port, node.ipaddr, (error) => {
+            if (error) {
+                logger.error(`Sender error: ${error.message}`);
+            }
+        });
     });
-});
-//}, PULSE_INTERVAL);
+}, PULSE_INTERVAL);
+*/ 
--- src/sender.ts
+++ src/sender.ts
@@ -15,8 +15,9 @@ process.on('message', (senderMessage: SenderMessage) => {
     pulseGroupMap.set(senderMessage.type, senderMessage.payload);
 });
 
+/*
 // Send same message to all nodes in nodelist
-//setInterval(() => {
+setInterval(() => {
     const nodeList: NodeAddress[] = pulseGroupMap.get(SenderPayloadType.NodeList)
     const outgoingMessage: string = pulseGroupMap.get(SenderPayloadType.OutgoingMessage)
     nodeList.forEach(function (node: NodeAddress) {
@@ -31,4 +32,5 @@ process.on('message', (senderMessage: SenderMessage) => {
             }
         });
     });
-//}, PULSE_INTERVAL);
+}, PULSE_INTERVAL);
+*/
