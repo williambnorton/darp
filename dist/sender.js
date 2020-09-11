0c6699c253dd6baeeb3a6596cabbf70652557287
William B. Norton <wbn@drpeering.net>
2020-09-10 17:02:16 -0700

    DARP Protocol with matrix and basic data graphs +

--- dist/sender.js
+++ dist/sender.js
@@ -13,19 +13,19 @@ process.on('message', function (senderMessage) {
     pulseGroupMap.set(senderMessage.type, senderMessage.payload);
 });
 // Send same message to all nodes in nodelist
-//setInterval(() => {
-var nodeList = pulseGroupMap.get(types_1.SenderPayloadType.NodeList);
-var outgoingMessage = pulseGroupMap.get(types_1.SenderPayloadType.OutgoingMessage);
-nodeList.forEach(function (node) {
-    var outgoingTimestamp = lib_1.now().toString();
-    var pulseMessage = outgoingTimestamp + "," + outgoingMessage;
-    var pulseBuffer = Buffer.from(pulseMessage, types_1.PulseMessageEncoding.latin1);
-    logger_1.logger.info("Sending " + pulseMessage + " to " + node.ipaddr + ":" + node.port);
-    console.log("Sending " + pulseMessage + " to " + node.ipaddr + ":" + node.port);
-    sender.send(pulseBuffer, 0, pulseBuffer.length, node.port, node.ipaddr, function (error) {
-        if (error) {
-            logger_1.logger.error("Sender error: " + error.message);
-        }
+setInterval(function () {
+    var nodeList = pulseGroupMap.get(types_1.SenderPayloadType.NodeList);
+    var outgoingMessage = pulseGroupMap.get(types_1.SenderPayloadType.OutgoingMessage);
+    nodeList.forEach(function (node) {
+        var outgoingTimestamp = lib_1.now().toString();
+        var pulseMessage = outgoingTimestamp + "," + outgoingMessage;
+        var pulseBuffer = Buffer.from(pulseMessage, types_1.PulseMessageEncoding.latin1);
+        logger_1.logger.info("Sending " + pulseMessage + " to " + node.ipaddr + ":" + node.port);
+        console.log("Sending " + pulseMessage + " to " + node.ipaddr + ":" + node.port);
+        sender.send(pulseBuffer, 0, pulseBuffer.length, node.port, node.ipaddr, function (error) {
+            if (error) {
+                logger_1.logger.error("Sender error: " + error.message);
+            }
+        });
     });
-});
-//}, PULSE_INTERVAL);
+}, PULSE_INTERVAL);
