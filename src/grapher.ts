ec32388e416a8ddfcd767aa3f43713f2bd965c6b
William B. Norton <wbn@drpeering.net>
2020-09-17 08:26:03 -0700

    DARP Protocol with matrix and basic data graphs +

--- src/grapher.ts
+++ src/grapher.ts
@@ -16,7 +16,7 @@ export function grapher(src: string, dest: string) {
 $(function() {
 	$(".chartContainer").CanvasJSChart({
 		title: {
-			text: "${src} → ${dest} ${myYYMMDD} "
+			text: "${src} -> ${dest} ${myYYMMDD} "
 		},
 		axisY: {
 			title: "latency in ms",
