47966bf71b05bef71773e02edf723915eef6bed1
William B. Norton <wbn@drpeering.net>
2020-09-25 17:16:48 -0700

    DARP Protocol with matrix and basic data graphs +

--- dist/pulsegroup.js
+++ dist/pulsegroup.js
@@ -873,9 +873,9 @@ var AugmentedPulseGroup = /** @class */ (function () {
                         incomingPulseEntry.history.shift(); // drop off the last sample
                     }
                     var filename = "../" + incomingPulse.geo + "-" + _this.mintTable[0].geo + ".medianHistory.json"; //once a minute peel off the median history and store for later grapher calls
-                    console.log("...concatentaing dataPoint sets " + incomingPulseEntry.medianHistory + " + " + incomingPulseEntry.history);
+                    //console.log(`...concatentaing dataPoint sets ${incomingPulseEntry.medianHistory} + ${incomingPulseEntry.history}`);
                     var dataPoints = incomingPulseEntry.medianHistory.concat(incomingPulseEntry.history);
-                    console.log("...writing dataPoints to " + filename + " : " + dataPoints);
+                    //console.log(`...writing dataPoints to ${filename} : ${dataPoints}`);
                     var str = JSON.stringify(dataPoints);
                     fs.writeFile(filename, str, function (err) {
                         if (err)
