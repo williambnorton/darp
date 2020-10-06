08e616a3cf79ebc8a6b4c89ac1271b0d3bb62be6
William B. Norton <wbn@drpeering.net>
2020-10-06 12:49:49 -0700

    DARP Protocol with matrix and basic data graphs +

--- bootdarp.bash
+++ bootdarp.bash
@@ -109,7 +109,7 @@ do
         kill `cat $DARPDIR/index.pid`
     fi
     echo `date` 'Starting DARP $VERSION ...'
-	node index > ../_DARP.log
+	node index > darp.log
 #	node index 
     rc=$?
     echo `date` "FINISHED DARP Protocol index.js done rc=$rc" #| tee -a NOIA.log
