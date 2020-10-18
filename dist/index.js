4e2179d44ac02ccb7cfcf63e4676e22dbea7f857
William B. Norton <wbn@drpeering.net>
2020-10-18 05:43:07 -0700

    DARP Protocol with matrix and basic data graphs +

--- dist/index.js
+++ dist/index.js
@@ -308,6 +308,7 @@ app.get('/nodefactory', function (req, res) {
         res.end(JSON.stringify(null));
         return;
     }
+    // WE are getting nodes coming in to nodeFactory of a sub. Could accept also?
     /* untested feture to redirectr rrequeat to group owner so a node can communicate with another only knowing their IP.
         if (myPulseGroup.groupOwner!=me.geo) {
             var redirectedURL='http://'+genesis.ipaddr+":"+genesis.port+req.originalUrl;
--- src/index.ts
+++ src/index.ts
@@ -322,7 +322,7 @@ app.get('/nodefactory', function(req, res) {
 
 
 
-
+// WE are getting nodes coming in to nodeFactory of a sub. Could accept also?
 
 /* untested feture to redirectr rrequeat to group owner so a node can communicate with another only knowing their IP.
     if (myPulseGroup.groupOwner!=me.geo) {
