1d2b9adb29b8720e5442362846cce1cbe706d48f
William B. Norton <wbn@drpeering.net>
2020-11-25 16:18:08 -0800

    DARP Protocol with matrix and basic data graphs +

--- dist/pulsegroup.js
+++ dist/pulsegroup.js
@@ -778,7 +778,7 @@ var AugmentedPulseGroup = /** @class */ (function () {
                     var genesisVersion = JSON.parse(body);
                     var mySWversion = lib_1.MYVERSION(); // find the Build.*
                     logger_1.logger.info("checkSWversion(): genesis SWversion==" + lib_1.dump(genesisVersion) + " MY SW Version=" + mySWversion + " me.version=" + _this.config.VERSION);
-                    console.log("checkSWversion(): genesis SWversion==" + genesisVersion + " MY SW Version=" + mySWversion + " me.version=" + _this.config.VERSION);
+                    //console.log(`checkSWversion(): genesis SWversion==${genesisVersion} MY SW Version=${mySWversion} me.version=${this.config.VERSION}`);
                     if (genesisVersion != mySWversion) {
                         var dockerVersion = genesisVersion.split(":")[0];
                         var myDockerVersion = mySWversion.split(":")[0];
