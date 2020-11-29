74ff36d7138cf6f16677708128d2cbcaa288ee3d
William B. Norton <wbn@drpeering.net>
2020-11-28 19:33:59 -0800

    DARP Protocol with matrix and basic data graphs +

--- dist/testport.js
+++ dist/testport.js
@@ -5,33 +5,54 @@
 //	$2 is optional port specified DEFAULT=65013
 //
 var myArgs = process.argv.slice(2);
-if (myArgs.length != 4) {
-    console.log("usage(): " + myArgs[0] + " GENESISIP GENESISPORT MYIP MYPORT");
-    console.log("Example: " + myArgs[0] + " 52.52.1.32 65013 `curl ifconfig.io` 65013");
+if (myArgs.length < 4) {
+    console.log("usage(): " + myArgs[0] + " MYIP MYPORT GENESISIP GENESISPORT [IP PORT ... IP PORT]");
+    console.log("Example: " + myArgs[0] + " `curl ifconfig.io` 65013 52.52.1.32 65013 ");
     process.exit(1);
 }
-var GENESISIP = myArgs[0];
-var GENESISPORT = myArgs[1];
-var MYIP = myArgs[2];
-var MYPORT = myArgs[3];
+var MYIP = myArgs[0];
+var MYPORT = myArgs[1];
+var GENESISIP = myArgs[2];
+var GENESISPORT = myArgs[3];
+var numberPings = 3;
 console.log("GENESISIP=" + GENESISIP + " GENESISPORT=" + GENESISPORT + " MYIP=" + MYIP + " MYPORT=" + MYPORT);
 var dgram = require('dgram');
 var client = dgram.createSocket('udp4');
+var done = false;
 client.on('listening', function () {
     var address = client.address();
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
 });
 client.on('message', function (message, remote) {
     console.log(remote.address + ':' + remote.port + ' - ' + message);
+    console.log("PORTS WORK");
+    //this proves the port works both directions
+    //here we might callback or somehow use the retrieved GENESISPUBLICKEY to prove it works
+    done = true;
 });
 function DARPping() {
-    var message = "11,11,11,11,11,11,11,11,11,11,11";
-    client.send(message, 0, message.length, GENESISPORT, GENESISIP, function (err, bytes) {
-        if (err)
-            throw err;
-        console.log('UDP message sent to ' + GENESISIP + ':' + GENESISPORT);
-    });
-    setTimeout(DARPping, 1000);
+    var timeNow = new Date();
+    var message = timeNow.getTime() + ",11,?,PUBLICKEY,11,11,11,11,11,11,couldSendHost";
+    var _loop_1 = function () {
+        var IP = myArgs[i];
+        var Port = myArgs[i + 1];
+        client.send(message, 0, message.length, Port, IP, function (err, bytes) {
+            if (err)
+                throw err;
+            console.log('UDP message sent to ' + IP + ':' + port);
+        });
+    };
+    for (var i = 2; i < myArgs.length; i = i + 2) {
+        _loop_1();
+    }
+    if (--numberPings == 0) {
+        console.log("FAILED");
+        process.exit(1);
+    }
+    if (done)
+        process.exit(0);
+    else
+        setTimeout(DARPping, 1000);
 }
 client.bind(MYPORT); //server listening 0.0.0.0:65013
 DARPping();
