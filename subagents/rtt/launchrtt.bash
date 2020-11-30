8c34d72515b6e1d2401c921d93cccd1433f2a9c7
William B. Norton <wbn@drpeering.net>
2020-11-30 13:17:07 -0800

    DARP Protocol with matrix and basic data graphs +

--- subagents/rtt/launchrtt.bash
+++ subagents/rtt/launchrtt.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo `date` "Starting $0 sub agent to measure round trip time "
+echo `date` "Starting $0 sub agent to measure round trip time - DELETEING PING measures and waiting 10 seconds for wireguard connections to come up"
 cd /root/darp/subagents/rtt/
+rm -f ip.*
 sleep 10
 while [ "" = "" ]; 
 do
