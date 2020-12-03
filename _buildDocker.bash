efb99e068eb83871b83d0e6302d72b21ef216e37
William B. Norton <wbn@drpeering.net>
2020-12-03 14:49:43 -0800

    DARP Protocol with matrix and basic data graphs +

--- _buildDocker.bash
+++ _buildDocker.bash
@@ -4,12 +4,12 @@
 rm Docker.??????.????
 date>"Docker."`date +%y%m%d.%H%M`
 ls -l Docker.*
-DOCKERVERSION=`ls Docker.*`
+DOCKERVERSION=`ls Docker.[0-9].*`
 
-echo `date` Building darp codebase 
+echo `date` Building darp docker $DOCKERVERSION
 ./builddarp.bash 
 if [ $? -ne 0 ]; then
-	echo `date` buildadrp failed
+	echo `date` builddarp failed
 	exit -1
 fi
 
@@ -17,7 +17,7 @@ npm install && npm update
 echo `date` Building the docker container
 docker build --no-cache -t williambnorton/darp:$DOCKERVERSION -t williambnorton/darp:testnet . && docker push williambnorton/darp
 #docker build --no-cache -t williambnorton/darp:$DOCKERVERSION . && docker push williambnorton/darp:$DOCKERVERSION
-echo `date` New docker $DOCKERVERSION running DARP `ls Build*`
+echo `date` New docker $DOCKERVERSION running DARP `ls Docker.[0-9].*`
 echo `date` Finished $0 Force genesis node to reload SW and the group will follow
 
 #./builddarp.bash              ####This deals with a timing issue - nodes keep reloading trying to get same SW as genesis, but it is not possible
