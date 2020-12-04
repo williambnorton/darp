988fec7d018feaed0012650f96aa695040d384e2
William B. Norton <wbn@drpeering.net>
2020-12-03 17:27:06 -0800

    DARP Protocol with matrix and basic data graphs +

--- updateSW.bash
+++ updateSW.bash
@@ -14,52 +14,38 @@ fi
     ls -l
     CURRENTDOCKER=`ls Docker.*`
     CURRENTDARP=`ls Build*`
-    echo `date` $0 "CURRENTDOCKER=$CURRENTDOCKER CURRENTDARP=$CURRENTDARP"
-    CURRENT=`ls Build*`
-    echo `date` "$0 Current SW is "`ls Docker.*`":"`ls Build*`
+    CURRENTVERSION="$CURRENTDOCKER:$CURRENTDARP"
+    echo `date` $0 "CURRENTDOCKER=$CURRENTDOCKER CURRENTDARP=$CURRENTDARP CURRENTVERSION=$CURRENTVERSION"
+
     cd /tmp
     rm -rf /tmp/darp
     #mv $DARPDIR /tmp/darp
     echo `date` "updateSW.bash: Cloning $BRANCH darp code from github"
     ( git clone --depth 1 --branch $BRANCH https://github.com/williambnorton/darp.git /tmp/darp 2>&1 ) #>/dev/null 
     cd /tmp/darp
-    NEW=`ls Build*`
+    NEWDARPVERSION=`ls Build*`
 
-   echo UPDATESW.BASH "$CURRENT" "$NEW" 
+   echo UPDATESW.BASH "$CURRENTDARP" "$NEWDARPVERSION" 
 
-   if [ "$CURRENT" == "$NEW" ]; then
+   if [ "$CURRENTDARP" == "$NEWDARPVERSION" ]; then
 	    echo `date` No Change       
     else
-        NEWDOCKERVERSION=`echo $NEW|awk -F: '{ print $1 }'`
-        NEWDARPVERSION=`echo $NEW|awk -F: '{ print $2 }'`
-        echo `date` "$0 Software changed. Was $CURRENT Now is $NEW NEWDOCKERVERSION=$NEWDOCKERVERSION NEWDARPVERSION=$NEWDARPVERSION"
+        echo `date` "$0 Software changed. Was $CURRENTDARP Now is $NEWDARPVERSION"
         cd /tmp/darp
         echo 'CLONED INTO /tmp/darp directory.'
         #echo Killing handlepulse to force reload: `ls $DARPDIR/*.pid`
         cd /tmp
-        echo `date` "moving current code into root directory"
+        echo `date` "moving CURRENTDARP code into root directory"
         SUFFIX=`date +%y%m%d.%H%M`
         mv $DARPDIR /tmp/darp.$SUFFIX
 
         mv darp $HOME
         cp -R /tmp/darp.$SUFFIX/node_modules /root/darp/node_modules  ### updateSW.bash BROKEN
-        echo `date` "New Code installed:"
-        cd $DARPDIR; ls
-        echo `date` "NEW Build - Build = $NEW"
-        rm Build.* Docker.*
+        rm Build* Docker.*
         echo $NEWDARPVERSION > $NEWDARPVERSION
-        echo $NEWDOCKERVERSION > $NEWDOCKERVERSION
     fi
 
-    echo `date` "Completed git clone into ~/darp - OLD=$CURRENT NEW=$NEW"
-        echo `date` "$0 Software changed. Was $CURRENT Now is $NEW NEWDOCKERVERSION=$NEWDOCKERVERSION NEWDARPVERSION=$NEWDARPVERSION"
-        echo `date` "$0 Software changed. Was $CURRENT Now is $NEW NEWDOCKERVERSION=$NEWDOCKERVERSION NEWDARPVERSION=$NEWDARPVERSION"
-        echo `date` "$0 Software changed. Was $CURRENT Now is $NEW NEWDOCKERVERSION=$NEWDOCKERVERSION NEWDARPVERSION=$NEWDARPVERSION"
-        echo `date` "$0 Software changed. Was $CURRENT Now is $NEW NEWDOCKERVERSION=$NEWDOCKERVERSION NEWDARPVERSION=$NEWDARPVERSION"
-        echo `date` "$0 Software changed. Was $CURRENT Now is $NEW NEWDOCKERVERSION=$NEWDOCKERVERSION NEWDARPVERSION=$NEWDARPVERSION"
-        echo `date` "$0 Software changed. Was $CURRENT Now is $NEW NEWDOCKERVERSION=$NEWDOCKERVERSION NEWDARPVERSION=$NEWDARPVERSION"
-        echo `date` "$0 Software changed. Was $CURRENT Now is $NEW NEWDOCKERVERSION=$NEWDOCKERVERSION NEWDARPVERSION=$NEWDARPVERSION"
-        echo `date` "$0 Software changed. Was $CURRENT Now is $NEW NEWDOCKERVERSION=$NEWDOCKERVERSION NEWDARPVERSION=$NEWDARPVERSION"
-        echo `date` "$0 Software changed. Was $CURRENT Now is $NEW NEWDOCKERVERSION=$NEWDOCKERVERSION NEWDARPVERSION=$NEWDARPVERSION"
-        echo `date` "$0 Software changed. Was $CURRENT Now is $NEW NEWDOCKERVERSION=$NEWDOCKERVERSION NEWDARPVERSION=$NEWDARPVERSION"
+    echo `date` "Completed git clone into ~/darp - OLD=$CURRENTDARP NEW=$NEW"
+        echo `date` "$0 Software changed. Was $CURRENTDARP Now NEWDARPVERSION=$NEWDARPVERSION"
+
     ls
