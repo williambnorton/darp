3f4ebfdccb8521eaff905aacf50afeaf87b64415
William B. Norton <wbn@drpeering.net>
2020-12-12 09:36:46 -0800

    DARP Protocol +

--- _buildDocker.bash
+++ _buildDocker.bash
@@ -19,7 +19,12 @@ echo `date` Building the docker container
 #
 #	this will push the :testnet :latest :Docker.YYMMDD.HHMM
 #
-docker build --no-cache -t williambnorton/darp:$DOCKERVERSION -t williambnorton/darp:testnet -t williambnorton/darp:latest . && docker push williambnorton/darp
+docker tag williambnorton/darp:$DOCKERVERSION
+docker tag williambnorton/darp:testnet
+docker tag williambnorton/darp:latest
+
+docker build --no-cache . && docker push williambnorton/darp
+#docker build --no-cache -t williambnorton/darp:$DOCKERVERSION -t williambnorton/darp:testnet -t williambnorton/darp:latest . && docker push williambnorton/darp
 #docker build --no-cache -t williambnorton/darp:$DOCKERVERSION . && docker push williambnorton/darp:$DOCKERVERSION
 END=`date +%s`
 DELTA=`expr $END - $START`
@@ -29,6 +34,12 @@ echo `date` Finished $0 build docker took $DELTA_MIN minutes to make this $DOCKE
 
 #./builddarp.bash              ####This deals with a timing issue - nodes keep reloading trying to get same SW as genesis, but it is not possible
 say "[[volm 0.05]] Bill, the docker build is complete. it took $DELTA_MIN minutes to complete."
+
+
+
+
+
+
 
 #ssh -i ~/PEM/AWS-US-WEST-1A.pem ubuntu@52.53.222.151 '(sleep 30;~/wireguard/wgwatch.bash)& docker rm -f $(docker ps -a -q);docker rmi -f $(docker images -q); docker run --rm -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e "HOSTNAME="`hostname`   -e "WALLET=auto"   -d williambnorton/darp:$DOCKERVERSION ' &
 #say "[[volm 0.2]]  I am currently relaunching the primary genesis node"
