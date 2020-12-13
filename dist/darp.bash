995913231ce44de72db07e5a93b7008d3dac9647
William B. Norton <wbn@drpeering.net>
2020-12-13 09:57:34 -0800

    DARP Protocol +

--- dist/darp.bash
+++ dist/darp.bash
@@ -36,7 +36,8 @@ if [ $wireguard_rc -eq 0 -a $docker_rc -eq 0 ]; then
         #   PORT=65013
         PORT=65013
         echo "docker run --rm -d -p ${PORT}:${PORT} -p ${PORT}:${PORT}/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e PORT=$PORT -e GENESIS=auto -e HOSTNAME=`hostname` -e WALLET=auto   williambnorton/darp:DOCKERTAG "
-        docker run --rm -d -p ${PORT}:${PORT} -p ${PORT}:${PORT}/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e PORT=$PORT -e GENESIS="auto" -e "HOSTNAME="`hostname` -e "WALLET=auto"   williambnorton/darp:DOCKERTAG 
+        #docker run --rm -d -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e PORT=$PORT -e GENESIS="auto" -e "HOSTNAME="`hostname` -e "WALLET=auto"   williambnorton/darp:DOCKERTAG 
+        docker run --rm -d -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e "HOSTNAME="`hostname` -e "WALLET=auto"   williambnorton/darp:DOCKERTAG 
         
         rc=$?
         if [ $? -eq 86 ]; then
