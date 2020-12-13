c631892c6dc60eda6f3b0e1d8312e3756836f721
William B. Norton <wbn@drpeering.net>
2020-12-13 08:21:34 -0800

    DARP Protocol +

--- _buildDocker.bash
+++ _buildDocker.bash
@@ -57,7 +57,7 @@ CMD='(sleep 30;~/wireguard/wgwatch.bash)& docker run --rm -p 65013:65013 -p 6501
 #ssh -i ~/PEM/AWS-US-WEST-1A.pem ubuntu@52.53.222.151 \'$CMD \' &
 
 echo `date` About to launch SR-WAN docker
-ssh -i ~/PEM/AWS-US-WEST-1A.pem ubuntu@52.53.222.151 "bash -c '(sleep 30;~/wireguard/wgwatch.bash)& nohup docker run --rm -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e HOSTNAME=PUBLIC-SR-WAN -e WALLET=auto -d williambnorton/darp:$DOCKERVERSION' " &
+ssh -i ~/PEM/AWS-US-WEST-1A.pem ubuntu@52.53.222.151 "bash -c '(sleep 30;~/wireguard/wgwatch.bash)& nohup docker run --rm -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e HOSTNAME=AWS-US-WEST-1A -e WALLET=auto -d williambnorton/darp:$DOCKERVERSION' " &
 
 echo `date` About to launch SR-WAN Instrumentation docker
 ssh -i ~/PEM/AWS-US-WEST-1A.pem ubuntu@52.53.222.151 "bash -c 'nohup docker run -p 80:80 -d williambnorton/srwan " &
