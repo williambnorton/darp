5d4490ac1cf44131d5b045f5d7e175f732f11376
William B. Norton <wbn@drpeering.net>
2020-12-15 05:42:21 -0800

    DARP Protocol +

--- builddarp.bash
+++ builddarp.bash
@@ -35,9 +35,11 @@ git add *.bash
 #git add . && git commit -m "$MESSAGE + " && git pull && git push
 git tag $BUILD_TAG
 #git add . && git commit -m "$MESSAGE + " && git pull && git push 
-git add . && git commit -m "$MESSAGE + " && git pull && git push --tags
+git add . && git commit -m "$MESSAGE + " && git pull && git push 
 
+echo RUNNING git push origin $BUILD_TAG
 git push origin $BUILD_TAG
+git push origin latest
 echo `date`" Completed compiles + git push for $BUILD_TAG "
 
 END=`date +%s`
