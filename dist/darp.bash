253b85d726e41f38bd69eec0de7e929230896fb4
William B. Norton <wbn@drpeering.net>
2020-12-16 05:25:12 -0800

    DARP Protocol +

--- dist/darp.bash
+++ dist/darp.bash
@@ -35,16 +35,18 @@ if [ $wireguard_rc -eq 0 -a $docker_rc -eq 0 ]; then
         #   WALLET=wallet to use to refill escrow of tokens, auto provides a limited # of tokens for demonstrating relaying traffic (for simulation)
         #   PORT=65013
         PORT=65013
-        echo "docker run --rm -d -p ${PORT}:${PORT} -p ${PORT}:${PORT}/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e PORT=$PORT -e GENESIS=auto -e HOSTNAME=`hostname` -e WALLET=auto   williambnorton/darp:DOCKERTAG "
+        #echo "docker run --rm -d -p ${PORT}:${PORT} -p ${PORT}:${PORT}/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e PORT=$PORT -e GENESIS=auto -e HOSTNAME=`hostname` -e WALLET=auto   williambnorton/darp:DOCKERTAG "
         #docker run --rm -d -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e PORT=$PORT -e GENESIS="auto" -e "HOSTNAME="`hostname` -e "WALLET=auto"   williambnorton/darp:DOCKERTAG 
-        docker run --rm -d -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e "HOSTNAME="`hostname` -e "WALLET=auto"   williambnorton/darp:DOCKERTAG 
         
+        echo `date` "darp.bash:  NEW TEST - directly running this DOCKER by tag"
+        docker run --rm -d -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e "HOSTNAME="`hostname` -e "WALLET=auto"   williambnorton/darp:DOCKERTAG      
         rc=$?
         if [ $? -eq 86 ]; then
             echo `date` "==================== EXIT DOCKER AND STOP ===================== docker EXITTED with rc==86"
             exit 86
         fi
-        echo `date` "$0 Docker exitted with rc=$rc- sleeping 15 seconds and fetching new docker and restarting"
+        echo `date` "$0 Docker exitted with rc=$rc - sleeping 15 seconds and fetching new docker and restarting"
+        
         sleep 15
         STATE=`cat ~/wireguard/STATE`
         echo `date` "$0 STATE=$STATE"
