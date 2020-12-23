727502f4452ed2b3028987e2eff4f7f1626418c6
William B. Norton <wbn@drpeering.net>
2020-12-23 07:02:08 -0800

    DARP Protocol

--- bootdarp.bash
+++ bootdarp.bash
@@ -97,8 +97,9 @@ do
     #echo `date` "***** GENESISNODESLIST=$GENESISNODELIST"
 
     #node scripts/testport.ts $MY_IP 65013 `cat awsgenesis.config genesis.config operators.config` >porttest.txt  #inclucde all
-    node scripts/testport.ts $MY_IP 65013 $GENESISNODELIST >porttest.txt
+    node scripts/testport.ts $MY_IP 65013 $GENESISNODELIST | grep ",Docker." >porttest.txt
     echo "*********************************     PORTS AVAILABLE TO CONNECT TO     **************************************" 
+
     cat porttest.txt
     echo "BEST CHOICE BY LATENCY"
     FIRST_LINE=`cat porttest.txt | head -1`
