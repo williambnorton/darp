d0dae8a9454a5c44d6bb9b1b4d4a4b22a61be5bb
William B. Norton <wbn@drpeering.net>
2020-12-24 08:42:04 -0800

    DARP Protocol

--- scripts/configWG.bash
+++ scripts/configWG.bash
@@ -6,9 +6,9 @@
 #
 if [ "$WGDIR" = "" ]; then WGDIR=/etc/wireguard; fi
 
-echo `date` "setting up our $WGDIR configuration directory for wireguard public keys"
+#echo `date` "setting up our $WGDIR configuration directory for wireguard public keys"
 if [ ! -d $WGDIR ]; then umask 077; mkdir -p $WGDIR; echo `date` "ERROR: Should not hve to but Created $WGDIR"; fi  #make sure wireguard directory exists
-echo `date` "$0 creating wireguard configuration in $WGDIR from $GENESIS"
+#echo `date` "$0 creating wireguard configuration in $WGDIR from $GENESIS"
 ls -l $WGDIR
 #chown ubuntu $WGDIR  #we know our docker runs ubuntu HACK breaks in native mode where no ubuntu exists
 #if [ $? -ne 0 ]; then sudo chown ubuntu $WGDIR; fi
@@ -18,7 +18,7 @@ ls -l $WGDIR
 touch $WGDIR
 cd $WGDIR
 umask 077
-echo `date` "This will create publickey and privatekey We do not want them to include space characters, braces or brackets or commas"
+#echo `date` "This will create publickey and privatekey We do not want them to include space characters, braces or brackets or commas"
 DONE=0
 while [ $DONE -eq 0 ]
 do
@@ -46,7 +46,7 @@ do
 done
 #PRIVATEKEY=`echo $PRIVATEKEY | sed '1,$s/=//g'`
 #PRIVATEKEY=`echo $PRIVATEKEY | sed '1,$s/=//g'`
-echo PRIVATEKEY=$PRIVATEKEY PUBLICKEY=$PUBLICKEY
+#echo PRIVATEKEY=$PRIVATEKEY PUBLICKEY=$PUBLICKEY
 #
 #	The wgbase model will be used to re-build the conf file
 # Note: for security reasons we don't NEED to store privatekey in VM
@@ -63,10 +63,10 @@ echo "# my PublicKey to share is $PUBLICKEY" >>$WGDIR/wgbase.conf
 echo "#" >>$WGDIR/wgbase.conf
 
 cp $WGDIR/wgbase.conf $WGDIR/darp0.conf
-echo `date` "Base wireguard config: darp0.conf " `cat $WGDIR/darp0.conf` 
+#echo `date` "Base wireguard config: darp0.conf " `cat $WGDIR/darp0.conf` 
 
-echo `date` "$0 wgbase.conf below - the rest will be added by running code"
-cat $WGDIR/wgbase.conf
+#echo `date` "$0 wgbase.conf below - the rest will be added by running code"
+#cat $WGDIR/wgbase.conf
 
 echo `date` "WGDIR=$WGDIR"
 cd
