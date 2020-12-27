65cf1725cc0c391ace0123a5e729c85520369fbb
William B. Norton <wbn@drpeering.net>
2020-12-27 08:05:24 -0800

    DARP Protocol

--- scripts/testport.ts
+++ scripts/testport.ts
@@ -46,7 +46,7 @@ function darpPing() {
         let Name=genesisNode.split(",")[2]
         var message=`${startTime.getTime()},11,${process.env.MY_SWVERSION},${process.env.MY_IP},${process.env.MY_PORT},${process.env.MY_GEO},${IP},${Port},${Name},${process.env.MY_IP},${process.env.MY_PORT},${process.env.MY_GEO}`; //specify GENESIS Node directly
         if ( IP == process.env.MY_IP ) message=message+",SELF"
-        console.log(`# Here we send DARP Ping to ${Name} ${IP}:${Port}`);
+        console.log(`# Here we send DARP Ping to ${Name} ${IP}:${Port} message=${message}`);
 
         client.send(message, 0, message.length, Port, IP, function(err, bytes) {
             if (err) throw err;
