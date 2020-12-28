48e262fa1dd32460cb60811b18fa953610c5bc03
William B. Norton <wbn@drpeering.net>
2020-12-28 15:19:33 -0800

    DARP Protocol

--- builddarp.bash
+++ builddarp.bash
@@ -32,23 +32,21 @@ rm -f subagents/rtt/ip*
 # build then copy compiled code into dist directory
 cd src;tsc *.ts;mv *.js ../dist/; cd ..
 
-#git add *.bash
-#git add . && git commit -m "$MESSAGE + " && git pull && git push
-#git add . && git commit -m "$MESSAGE + " && git pull && git push 
 git add . && git commit -m "$MESSAGE" && git pull && git push 
 
 echo deleting latest
-#:git push --delete origin latest
 git push origin :latest
+
 echo `date` deleted latest tag in github repo so now tagging latest and pushing
 sleep 1
 #git push origin $BUILD_TAG
 #git tag $BUILD_TAG
-git tag latest
 git push origin $BUILD_TAG
 echo `date`" Completed git push for $BUILD_TAG "
+
 git push origin latest
 echo `date`" Completed git push for latest "
+
 END=`date +%s`
 DELTA=`expr $END - $START`
 DELTA_MIN=`expr $DELTA / 60`
