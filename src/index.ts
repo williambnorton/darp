cad3e250c2db19fe72c8f3706d49986fd4b52553
William B. Norton <wbn@drpeering.net>
2021-01-02 16:07:17 -0800

    DARP Protocol

--- src/index.ts
+++ src/index.ts
@@ -420,13 +420,13 @@ app.get('/nodefactory', function(req, res) {
             const me = new MintEntry(1, config.GEO, config.PORT, config.IP, config.PUBLICKEY, config.VERSION, config.WALLET, config.BOOTTIMESTAMP);  //All nodes can count on 'me' always being present
             const megenesis = new MintEntry(1, config.GEO, config.PORT, config.IP, config.PUBLICKEY, config.VERSION, config.WALLET, config.BOOTTIMESTAMP);  //All nodes also start out ready to be a genesis node for others
             var pulse = new PulseEntry(1, config.GEO, config.GEO+".1", config.IP, config.PORT, config.VERSION, config.BOOTTIMESTAMP);    //makePulseEntry(mint, geo, group, ipaddr, port, version) 
-            var myPulseGroup = new PulseGroup(me, megenesis, pulse);  //my pulseGroup Configuration, these two me and genesis are the start of the mintTable
-            myPulseGroups[ config.GEO+".1" ] = myPulseGroup;  //@WBNWBNWBN
-            console.log(`myPulseGroup=${JSON.stringify(myPulseGroup,null,2)}`);
+            var mePulseGroup = new PulseGroup(me, megenesis, pulse);  //my pulseGroup Configuration, these two me and genesis are the start of the mintTable
+            myPulseGroups[ config.GEO+".1" ] = mePulseGroup;  //@WBNWBNWBN
+            console.log(`mePulseGroup=${JSON.stringify(mePulseGroup,null,2)}`);
             //return;
         } else {
-            myPulseGroup = myPulseGroups[ config.GEO+".1" ]   //we work on this newly formed pulseGorup of ours
         }
+        myPulseGroup = myPulseGroups[ config.GEO+".1" ]   //we work on this newly formed pulseGorup of ours
         console.log(`continuing on to nodeFactory`);
     }
 
