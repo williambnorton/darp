a67f2677341eb18aec9cf423af3d601b92f21fc3
William B. Norton <wbn@drpeering.net>
2021-01-15 06:12:59 -0800

    DARP Protocol

---
+++ src/receiver.ts
@@ -0,0 +1,36 @@
+/** @module receiver listens to messages from a group of nodes */
+
+/*
+import dgram = require("dgram");
+import { now } from "./lib";
+import { logger, LogLevel } from "./logger";
+import { PulseMessageEncoding } from "./types";
+
+logger.setLevel(LogLevel.ERROR);
+
+const LISTENING_PORT = parseInt(process.argv[2]);
+const receiver = dgram.createSocket("udp4");
+
+receiver.on("error", (err) => {
+    logger.error(`Receiver error:\n${err.stack}`);
+    receiver.close();
+});
+
+receiver.on("listening", () => {
+    const address = receiver.address();
+    logger.info(`Receiver listening ${address.address}:${address.port}`);
+});
+
+receiver.on("message", (pulseBuffer, rinfo) => {
+    const incomingTimestamp = now().toString();
+    logger.info(`Received ${pulseBuffer} from ${rinfo.address}:${rinfo.port}`);
+    // prepend our timeStamp
+    const incomingMessage = incomingTimestamp + "," + pulseBuffer.toString(PulseMessageEncoding.latin1);
+    if (typeof process.send !== "undefined") {
+        process.send(incomingMessage);
+    }
+});
+
+receiver.bind(LISTENING_PORT);
+
+*/
