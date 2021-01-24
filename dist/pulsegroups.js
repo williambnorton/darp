d4154881e6fec1a084234991ff47fdad0c3c3848
William B. Norton <wbn@drpeering.net>
2021-01-24 14:40:27 -0800

    DARP Protocol

--- dist/pulsegroups.js
+++ dist/pulsegroups.js
@@ -23,6 +23,7 @@ exports.addPulseGroup = addPulseGroup;
 //
 var dgram = require("dgram");
 var receiver = dgram.createSocket("udp4");
+var udp = dgram.createSocket("udp4");
 receiver.on("error", function (err) {
     console.log("Receiver error:\n" + err);
     receiver.close();
@@ -62,8 +63,44 @@ receiver.on("message", function (pulseBuffer, rinfo) {
         console.log("" + lib_1.dump(exports.myPulseGroups));
     }
     else {
+        var incomingPulseGroup = exports.myPulseGroups[incomingPulse.group];
+        if (incomingPulse.msgType == "11") {
+            //console.log(`incomingPulse DARP PING (testport)`); // request=${JSON.stringify(incomingPulse)}`);
+            console.log("PING MESSAGE incomingPulse.msgType=" + incomingPulse.msgType + "    incomingPulse=" + JSON.stringify(incomingPulse, null, 2));
+            //
+            //if (this.isGenesisNode() && this.nodeCount<this.config.MAXNODES) {
+            //                    if ( this.nodeCount<this.config.MAXNODES) {
+            //HERE put the nodeCount and the # better paths
+            //PONG MESSAGE
+            var message = lib_1.now() + ",12," + incomingPulseGroup.mintTable[0].version + "," + incomingPulseGroup.mintTable[0].ipaddr + "," + incomingPulseGroup.mintTable[0].port + "," + incomingPulseGroup.mintTable[0].geo + "," + incomingPulseGroup.mintTable[0].bootTimestamp + "," + incomingPulseGroup.mintTable[0].publickey;
+            //else
+            //    var message="http://"+this.config.GENESIS+":"+this.config.GENESISPORT+"/darp.bash?pongMsg="+pongMsgEncoded;
+            console.log("Sending PONG (12) to " + rinfo.address + ":65013 message=" + message);
+            udp.send(message, 65013, rinfo.address);
+            //                    } else {
+            //                        console.log(`pulseGroup full - not answering request to join... `);
+            //                    }
+            //
+            //
+            // STILL DEVELOPING THIS AREA -- PING should include stuff to allow receiver to decide if it is a better connection for it
+            //  PONG should include enough to advocate the desired outcome - connect to me, to my genesis node, to this obne closer to you.
+            //
+            //
+        }
+        else {
+            //console.log(`incomingPulse.msgType=${incomingPulse.msgType}`);
+            if (parseInt(incomingPulse.msgType) == 12) { //PONG response
+                //console.log(`INCOMING DARP PONG (12).... incomingPulse.msgType=${incomingPulse.msgType}`);
+                //console.log(`pulsegroup.ts: PONG RESPONSE: ${JSON.stringify(incomingPulse,null,2)}`);
+            }
+            else { //default pass up the stack
+                console.log("INCOMING DARP PING RESPONSE incomingPulse.msgType=" + incomingPulse.msgType);
+                incomingPulseGroup.processIncomingPulse(incomingPulse);
+            }
+            return; //DARP Ping processed
+        }
         console.log("pulseGroup received " + (incomingPulse.geo + ":" + incomingPulse.group) + " message");
-        exports.myPulseGroups[incomingPulse.group].recvPulses(incomingMessage, rinfo.address, rinfo.port);
+        exports.myPulseGroups[incomingPulse.group].processIncomingPulse(incomingPulse);
     }
 });
 receiver.bind(65013);
--- src/pulsegroups.ts
+++ src/pulsegroups.ts
@@ -27,6 +27,8 @@ export function addPulseGroup(pulseGroup:PulseGroup) {
     var dgram = require("dgram");
 
     const receiver = dgram.createSocket("udp4");
+    const udp = dgram.createSocket("udp4");    
+
 
     receiver.on("error", (err:string) => {
         console.log(`Receiver error:\n${err}`);
@@ -70,9 +72,53 @@ export function addPulseGroup(pulseGroup:PulseGroup) {
             console.log(`unknown group pulse: ${incomingPulse.group}`);
             console.log(`${dump(myPulseGroups)}`);
         } else {
+            var incomingPulseGroup=myPulseGroups[incomingPulse.group];
+
+
+            if (incomingPulse.msgType=="11") {
+                    //console.log(`incomingPulse DARP PING (testport)`); // request=${JSON.stringify(incomingPulse)}`);
+                console.log(`PING MESSAGE incomingPulse.msgType=${incomingPulse.msgType}    incomingPulse=${JSON.stringify(incomingPulse,null,2)}`);
+                    //
+                    //if (this.isGenesisNode() && this.nodeCount<this.config.MAXNODES) {
+//                    if ( this.nodeCount<this.config.MAXNODES) {
+                            //HERE put the nodeCount and the # better paths
+                        //PONG MESSAGE
+                var message=`${now()},12,${incomingPulseGroup.mintTable[0].version},${incomingPulseGroup.mintTable[0].ipaddr},${incomingPulseGroup.mintTable[0].port},${incomingPulseGroup.mintTable[0].geo},${incomingPulseGroup.mintTable[0].bootTimestamp},${incomingPulseGroup.mintTable[0].publickey}` 
+
+                        //else
+                        //    var message="http://"+this.config.GENESIS+":"+this.config.GENESISPORT+"/darp.bash?pongMsg="+pongMsgEncoded;
+
+                console.log(`Sending PONG (12) to ${rinfo.address}:65013 message=${message}`);
+                udp.send(message, 65013, rinfo.address);
+//                    } else {
+//                        console.log(`pulseGroup full - not answering request to join... `);
+//                    }
+                    //
+                    //
+                    // STILL DEVELOPING THIS AREA -- PING should include stuff to allow receiver to decide if it is a better connection for it
+                    //  PONG should include enough to advocate the desired outcome - connect to me, to my genesis node, to this obne closer to you.
+                    //
+                    //
+            } else {
+                    //console.log(`incomingPulse.msgType=${incomingPulse.msgType}`);
+                if (parseInt(incomingPulse.msgType)==12) {    //PONG response
+                        //console.log(`INCOMING DARP PONG (12).... incomingPulse.msgType=${incomingPulse.msgType}`);
+                        //console.log(`pulsegroup.ts: PONG RESPONSE: ${JSON.stringify(incomingPulse,null,2)}`);
+                } else {  //default pass up the stack
+                    console.log(`INCOMING DARP PING RESPONSE incomingPulse.msgType=${incomingPulse.msgType}`);
+                    incomingPulseGroup.processIncomingPulse(incomingPulse);
+                }
+
+                return; //DARP Ping processed
+            }
+
+                                
             console.log(`pulseGroup received ${incomingPulse.geo+":"+incomingPulse.group} message`);
-            myPulseGroups[incomingPulse.group].recvPulses(incomingMessage,rinfo.address,rinfo.port);
+            myPulseGroups[incomingPulse.group].processIncomingPulse(incomingPulse);
         }
+
+
+
     });
 
     receiver.bind(65013);
