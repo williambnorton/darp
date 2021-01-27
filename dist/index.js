82c5552d77ed60d41d6d0415304b8c3a056d4df0
William B. Norton <wbn@drpeering.net>
2021-01-27 06:53:52 -0800

    DARP Protocol

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,40 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 exports.__esModule = true;
 /** entry point */
 /*  ALPHA  CODE  */
@@ -426,44 +462,46 @@ app.get('/nodefactory', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(newNodePulseGroup)); // send mint:0 mint:1 *mint:N groupEntry *entryN
 });
-/*
-// Initiate the protocol
+// Initiate the protocol  
 //  this is where it all begins - here we start up our own group
 // do this also for a group created for children
-(async () => {
-    try {
-        var myOriginalPulseGroup=myPulseGroup
-        myPulseGroup = await getPulseGroup(config);   //replaces starting myPulseGroup
-        //var anchorPulseGroup = await getPulseGroup(config);   //t
-
-        console.log(`asynch() DARP NODE STARTED: anchor GENESIS=${myPulseGroup.groupOwner} pulseGroup=${dump(myPulseGroup)}`);
-        var augmentedPulseGroup = new AugmentedPulseGroup(myPulseGroup);   //augmented with pulseGroup methods
-
-        //console.log(`augmentedPulseGroup=${JSON.stringify(augmentedPulseGroup,null,2)}`);
-        
-        augmentedPulseGroup.flashWireguard();  // create our wireguard files based on our mint Table
-        augmentedPulseGroup.pulse();
-        //augmentedPulseGroup.workerThread();  //start workerthread to asynchronously processes pulse messages
-        setTimeout(augmentedPulseGroup.findEfficiencies,1000);  //find where better paths exist between intermediaries - wait a second
-        setTimeout(augmentedPulseGroup.checkSWversion, 10 * 1000);  // check that we have the best software
-        setTimeout(augmentedPulseGroup.measurertt, 2 * 1000); // ping across wireguard every other second
-
-        myPulseGroups[ myPulseGroup.groupName ] = augmentedPulseGroup;     //wire it in
-
-
-        if (myPulseGroup.groupOwner  != me.geo ) {
-            myPulseGroups[ me.geo+".1" ] = new AugmentedPulseGroup(myOriginalPulseGroup);
-            console.log(`index.ts:  WE LAUNCHED OUR OWN PULSE GROUP ${JSON.stringify(myPulseGroups[ me.geo+".1" ],null,2) }`);
+(function () { return __awaiter(void 0, void 0, void 0, function () {
+    var myOriginalPulseGroup, augmentedPulseGroup, error_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                myOriginalPulseGroup = myPulseGroup;
+                return [4 /*yield*/, pulsegroup_1.getPulseGroup(config)];
+            case 1:
+                myPulseGroup = _a.sent(); //replaces starting myPulseGroup
+                //var anchorPulseGroup = await getPulseGroup(config);   //t
+                console.log("asynch() DARP NODE STARTED: anchor GENESIS=" + myPulseGroup.groupOwner + " pulseGroup=" + lib_1.dump(myPulseGroup));
+                augmentedPulseGroup = new pulsegroup_1.AugmentedPulseGroup(myPulseGroup);
+                //console.log(`augmentedPulseGroup=${JSON.stringify(augmentedPulseGroup,null,2)}`);
+                augmentedPulseGroup.flashWireguard(); // create our wireguard files based on our mint Table
+                augmentedPulseGroup.pulse();
+                //augmentedPulseGroup.workerThread();  //start workerthread to asynchronously processes pulse messages
+                setTimeout(augmentedPulseGroup.findEfficiencies, 1000); //find where better paths exist between intermediaries - wait a second 
+                setTimeout(augmentedPulseGroup.checkSWversion, 10 * 1000); // check that we have the best software
+                setTimeout(augmentedPulseGroup.measurertt, 2 * 1000); // ping across wireguard every other second
+                pulsegroups_1.myPulseGroups[myPulseGroup.groupName] = augmentedPulseGroup; //wire it in
+                if (myPulseGroup.groupOwner != me.geo) {
+                    pulsegroups_1.myPulseGroups[me.geo + ".1"] = new pulsegroup_1.AugmentedPulseGroup(myOriginalPulseGroup);
+                    console.log("index.ts:  WE LAUNCHED OUR OWN PULSE GROUP " + JSON.stringify(pulsegroups_1.myPulseGroups[me.geo + ".1"], null, 2));
+                }
+                //could clone this new pulseGroup as my own for accepting new connections
+                console.log("index.ts:    launching------>       myPulseGroups=" + JSON.stringify(pulsegroups_1.myPulseGroups, null, 2));
+                return [3 /*break*/, 3];
+            case 2:
+                error_1 = _a.sent();
+                logger_1.logger.error(error_1);
+                return [3 /*break*/, 3];
+            case 3: return [2 /*return*/];
         }
-
-        //could clone this new pulseGroup as my own for accepting new connections
-
-        console.log(`index.ts:    launching------>       myPulseGroups=${JSON.stringify(myPulseGroups,null,2)}`);
-    } catch (error) {
-        logger.error(error);
-    }
-})();
-*/
+    });
+}); })();
+/**/
 //
 //  darp.bash substitutes in proper CODE and CONFIG for new node
 //
