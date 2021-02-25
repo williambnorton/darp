404c0e5d4cca5e816985b40d74fde71f86cceb2e
William B. Norton <wbn@drpeering.net>
2021-02-25 12:02:14 -0800

    DARP Protocol

--- startAll.bash
+++ startAll.bash
@@ -17,6 +17,9 @@ do
 	port=`echo $node|awk -F, '{print $2}'`
 	name=`echo $node|awk -F, '{print $3}'`
 
+	if [ -f /tmp/x ]; then
+
+
 	if [ "$ip" != "" -a "$port" != "" -a "$name" != "" ]; then
 		echo $0 | grep start >/dev/null   #Is this a startAll.bash or stopAll.bash ?
 		if [ $? -eq 0 ]; then
@@ -26,8 +29,14 @@ do
 		else 
 			echo  `date` "REBOOTING GENESIS NODE /tmp/$$ ~/scripts/USR1 ubuntu@$ip $name $ip $port & "
 			echo ~/scripts/USR2 ubuntu@$ip $name $ip $port 
-			~/scripts/USR2 ubuntu@$ip $name $ip $port &
+			~/scripts/USR2 ubuntu@$ip $name $ip $port >>/tmp/x & 
 		fi
+	fi
+	else 
+		echo FIRST GENESIS NODE 
+		echo FIRST GENESIS NODE >/tmp/x
+		 echo "ssh -i ~/PEM/$name.pem ubuntu@$ip" '(sleep 30; docker run -p 80:80 -d williambnorton/srwan ) & (sleep 30; docker run --network="host" --restart=on-failure:10 --cap-add=NET_ADMIN --cap-add=SYS_MODULE -v /var/run/docker.sock:/var/run/docker.sock:ro --device /dev/net/tun:/dev/net/tun --name=syntropy-agent -e SYNTROPY_NETWORK_API=docker -e SYNTROPY_API_KEY=YUeDgQNAg4qbOSL0kA2gIUyiehoB1kLC -d syntropynet/agent:stable ) & (sleep 30;~/wireguard/wgwatch.bash) &  docker rm -f $(docker ps -a -q);docker rmi -f $(docker images -q); docker run --rm -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e "HOSTNAME="`hostname`   -e "WALLET=auto"   -it williambnorton/darp &'
+
 	fi
 
 done
