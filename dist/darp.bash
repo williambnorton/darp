2baabb4171ec776399ca06d9724532ee15b596a3
William B. Norton <wbn@drpeering.net>
2021-03-04 06:27:58 -0800

    DARP Protocol

--- dist/darp.bash
+++ dist/darp.bash
@@ -46,8 +46,16 @@ if [ $wireguard_rc -eq 0 -a $docker_rc -eq 0 ]; then
         export GENESISNODELIST="GENESIS_NODE_LIST"  #your port dedicated to DARP be configured (65013 is default)
         echo "GENESISNODELIST="$GENESISNODELIST #
 
+        //
+        //  Distribute Docker out of this Genesis Node instead of DockerHub - BW charges could be big, seems slower
+        //
         echo `date` "loading DARPDOCKER from http://_MY_IP:_MY_PORT/darpdocker "
+        STARTTIME=`date +%s`
         curl -o - http://_MY_IP:_MY_PORT/darpdocker | docker load   #fetch the docker from the running docker I connected to (instead of from docker hub)
+        ENDTIME=`date +%s`
+        ELAPSEDTIME=`expr $ENDTIME - $STARTTIME`
+        ELAPSEDSECONDS=`expr $ELAPSEDTIME / 1000`
+        echo `date` Download from http://_MY_IP:_MY_PORT/darpdocker took $ELAPSEDTIME seconds
         echo $0 'RUNNING: docker run --rm -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e "HOSTNAME="`hostname` -e "WALLET=auto"   williambnorton/darp:DOCKERTAG      '
         
         docker run --rm -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e "HOSTNAME="`hostname` -e "WALLET=auto"   williambnorton/darp:DOCKERTAG      
