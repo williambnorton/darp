805ba2b566ca19a96dd499f35450e72afffd2fd3
William B. Norton <wbn@drpeering.net>
2021-05-23 10:36:50 -0700

    DARP Protocol

--- startAll.bash
+++ startAll.bash
@@ -24,8 +24,8 @@ do
 			echo $0 | grep start >/dev/null   #Is this a startAll.bash or stopAll.bash ?
 			if [ $? -eq 0 ]; then
 				echo  `date` "LAUNCHING GENESIS NODE /tmp/$$ ~/scripts/USR1 ubuntu@$ip $name $ip $port & "
-				#~/scripts/USR1 ubuntu@$ip $name $ip $port  &
-				(~/scripts/USR1 ubuntu@$ip $name $ip $port 2>&1 ) >>/tmp/x  &
+				~/scripts/USR1 ubuntu@$ip $name $ip $port  &
+				#(~/scripts/USR1 ubuntu@$ip $name $ip $port 2>&1 ) >>/tmp/x  &
 			else 
 				echo  `date` "REBOOTING GENESIS NODE /tmp/$$ ~/scripts/USR1 ubuntu@$ip $name $ip $port & "
 				echo ~/scripts/USR2 ubuntu@$ip $name $ip $port 
@@ -38,7 +38,7 @@ do
 		 echo "ssh -i ~/PEM/$name.pem ubuntu@$ip" '(sleep 30; docker run -p 80:80 -d williambnorton/srwan ) & (sleep 30; docker run --network="host" --restart=on-failure:10 --cap-add=NET_ADMIN --cap-add=SYS_MODULE -v /var/run/docker.sock:/var/run/docker.sock:ro --device /dev/net/tun:/dev/net/tun --name=syntropy-agent -e SYNTROPY_NETWORK_API=docker -e SYNTROPY_API_KEY=YUeDgQNAg4qbOSL0kA2gIUyiehoB1kLC -d syntropynet/agent:stable ) & (sleep 30;~/wireguard/wgwatch.bash) &  docker rm -f $(docker ps -a -q);docker rmi -f $(docker images -q); docker run --rm -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e "HOSTNAME="`hostname`   -e "WALLET=auto"   -it williambnorton/darp &'
 
 	fi
-	sleep 5
+	#sleep 5
 
 done
 
@@ -57,13 +57,13 @@ do
 
 		if [ $? -eq 0 ]; then
 			echo  `date` "LAUNCHING non-genesis node /tmp/$$ ~/scripts/USR1 ubuntu@$ip $name $ip $port & "
-			#~/scripts/USR1 ubuntu@$ip $name $ip $port &
-			(~/scripts/USR1 ubuntu@$ip $name $ip $port 2>&1 ) >>/tmp/x  &
+			~/scripts/USR1 ubuntu@$ip $name $ip $port &
+			#(~/scripts/USR1 ubuntu@$ip $name $ip $port 2>&1 ) >>/tmp/x  &
 		else 
 			echo  `date` "REBOOTING non-genesis node /tmp/$$ ~/scripts/USR1 ubuntu@$ip $name $ip $port & "
 			echo ~/scripts/USR2 ubuntu@$ip $name $ip $port 
 			~/scripts/USR2 ubuntu@$ip $name $ip $port &
 		fi
 	fi
-	sleep 5
+	#sleep 5
 done
