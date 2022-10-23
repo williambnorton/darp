b81d6212df0288953846b91b7e12f6b64553c0ef
William B. Norton <bill.norton@gmail.com>
2022-10-23 09:11:33 -0700

    DARP Protocol

--- _buildDocker.bash
+++ _buildDocker.bash
@@ -4,10 +4,10 @@
 rm Docker.??????.????
 date > "Docker."`date +%y%m%d.%H%M`
 ls -l Docker.*
-DOCKERVERSION=`ls Docker.*`
+DARP_DOCKER_VERSION=`ls Docker.*`
 START=`date +%s`
 
-echo `date` $0 Building darp docker $DOCKERVERSION START=$START
+echo `date` $0 Building darp docker $DARP_DOCKER_VERSION START=$START
 ./builddarp.bash --noRelaunch
 if [ $? -ne 0 ]; then
 	echo `date` builddarp failed
@@ -19,39 +19,39 @@ echo `date` Building the docker container
 #
 #	this will push the tagged image : Docker.YYMMDD.HHMM
 #
-#docker tag williambnorton/darp:$DOCKERVERSION
+#docker tag williambnorton/darp:$DARP_DOCKER_VERSION
 
 
 
 
-docker build --no-cache -t williambnorton/darp:latest -t williambnorton/darp:$DOCKERVERSION . && docker push williambnorton/darp:$DOCKERVERSION
+docker build --no-cache -t williambnorton/darp:latest -t williambnorton/darp:$DARP_DOCKER_VERSION . && docker push williambnorton/darp:$DARP_DOCKER_VERSION
 docker push williambnorton/darp:latest
 
 
 
 
-#docker build --no-cache -t williambnorton/darp:$DOCKERVERSION -t williambnorton/darp:testnet -t williambnorton/darp:latest . && docker push williambnorton/darp
-#docker build --no-cache -t williambnorton/darp:$DOCKERVERSION . && docker push williambnorton/darp:$DOCKERVERSION
+#docker build --no-cache -t williambnorton/darp:$DARP_DOCKER_VERSION -t williambnorton/darp:testnet -t williambnorton/darp:latest . && docker push williambnorton/darp
+#docker build --no-cache -t williambnorton/darp:$DARP_DOCKER_VERSION . && docker push williambnorton/darp:$DARP_DOCKER_VERSION
 END=`date +%s`
 DELTA=`expr $END - $START`
 DELTA_MIN=`expr $DELTA / 60`
-echo `date` New docker $DOCKERVERSION running DARP `ls Docker.*` START=$START END=$END DELTA=$DELTA
-echo `date` Finished $0 build docker took $DELTA_MIN minutes to make this $DOCKERVERSION docker
+echo `date` New docker $DARP_DOCKER_VERSION running DARP `ls Docker.*` START=$START END=$END DELTA=$DELTA
+echo `date` Finished $0 build docker took $DELTA_MIN minutes to make this $DARP_DOCKER_VERSION docker
 
 #./builddarp.bash              ####This deals with a timing issue - nodes keep reloading trying to get same SW as genesis, but it is not possible
 say "[[ volm 0.05 ]] Bill, the docker build is complete. it took $DELTA_MIN minutes."
 
 
-#ssh -i ~/PEM/AWS-US-WEST-1A.pem ubuntu@52.53.222.151 '(sleep 30;~/wireguard/wgwatch.bash)& docker rm -f $(docker ps -a -q);docker rmi -f $(docker images -q); docker run --rm -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e "HOSTNAME="`hostname`   -e "WALLET=auto"   -d williambnorton/darp:$DOCKERVERSION ' &
+#ssh -i ~/PEM/AWS-US-WEST-1A.pem ubuntu@52.53.222.151 '(sleep 30;~/wireguard/wgwatch.bash)& docker rm -f $(docker ps -a -q);docker rmi -f $(docker images -q); docker run --rm -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e "HOSTNAME="`hostname`   -e "WALLET=auto"   -d williambnorton/darp:$DARP_DOCKER_VERSION ' &
 #echo 'About to kill previous dockers and remove the images'
 #ssh -i ~/PEM/AWS-US-WEST-1A.pem ubuntu@52.53.222.151  'docker rm -f $(docker ps -a -q);docker rmi -f $(docker images -q);'
 
-echo `date` About to launch SR-WAN docker $DOCKERVERSION   #this is for the first node in the genesis list - static for now
-#ssh -i ~/PEM/AWS-US-WEST-1A.pem ubuntu@52.53.222.151 "bash -c '(sleep 30;~/wireguard/wgwatch.bash)& nohup docker run --rm -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e HOSTNAME=AWS-US-WEST-1A -e WALLET=auto -d williambnorton/darp:$DOCKERVERSION' " &
+echo `date` About to launch SR-WAN docker $DARP_DOCKER_VERSION   #this is for the first node in the genesis list - static for now
+#ssh -i ~/PEM/AWS-US-WEST-1A.pem ubuntu@52.53.222.151 "bash -c '(sleep 30;~/wireguard/wgwatch.bash)& nohup docker run --rm -p 65013:65013 -p 65013:65013/udp  -e PUID=1000 -e PGID=1000 -v ~/wireguard:/etc/wireguard  -e HOSTNAME=AWS-US-WEST-1A -e WALLET=auto -d williambnorton/darp:$DARP_DOCKER_VERSION' " &
 
 #echo `date` About to launch SR-WAN Instrumentation docker
-echo `date` $0 $DOCKERVERSION V=$V COMPLETE
-V=`echo $DOCKERVERSION|awk -F. '{ print $3 }'| sed 's/.\{1\}/& /g'`
+echo `date` $0 $DARP_DOCKER_VERSION V=$V COMPLETE
+V=`echo $DARP_DOCKER_VERSION|awk -F. '{ print $3 }'| sed 's/.\{1\}/& /g'`
 export DOCKER_BASE_PORT=65013
 if [ -f DOCKER_BASE_PORT ]; then
 	export DOCKER_BASE_PORT=`cat DOCKER_BASE_PORT`
